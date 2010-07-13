3e318ced0fdae0b40620660bb6ab45a1d2b51b71
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2010-07-13 18:29:08 +0000

    replace GNU make with Python

--- wrims/wrims1_workspace/wrims1/calsim/engine/build.py
+++ wrims/wrims1_workspace/wrims1/calsim/engine/build.py
@@ -11,6 +11,30 @@ subprocess.call(['clean.bat' ])
 subprocess.call(['cmd','/c','am' ])
 subprocess.call(['install.bat' ])
 
+newd = cwd+"\\Simsolver";
+print(newd);
+os.chdir(newd)
+
+subprocess.call(['clean.bat' ])
+subprocess.call(['cmd','/c','am' ])
+subprocess.call(['install.bat' ])
+
+newd = cwd+"\\calsim_main";
+print(newd);
+os.chdir(newd)
+
+subprocess.call(['clean.bat' ])
+subprocess.call(['cmd','/c','am' ])
+subprocess.call(['install.bat' ])
+
+newd = cwd+"\\mkcode";
+print(newd);
+os.chdir(newd)
+
+subprocess.call(['clean.bat' ])
+subprocess.call(['cmd','/c','am' ])
+subprocess.call(['install.bat' ])
+
 cwd2 = os.getcwd();
 print(cwd2);
 
