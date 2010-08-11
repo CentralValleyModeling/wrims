5942769cb2e52caf0cdbf18f255c1ed7fba03502
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2010-08-11 17:57:36 +0000

    test

--- wrims/wrims1_workspace/wrims1/calsim/version_generate.py
+++ wrims/wrims1_workspace/wrims1/calsim/version_generate.py
@@ -6,6 +6,7 @@ print __file__
 
 Version_ = 'WRIMS v1.3.4 beta (XA16)'
 Version_Nospace_ = 'WRIMS_v1.3.4_beta_XA16'
+Version_XA = 'xav13'
 
 #VersionTemplate     = "      character*16 :: dsm2_version = '8.0.4', svn_build = '@{Version_SVN}' " 
 VersionTemplate     = "version=@{Version} SVN:@{Version_SVN} " 
@@ -25,10 +26,20 @@ setupScriptTemplate =   '[Setup] \n' \
 						'Name:   "{group}\\Run @{Version} "; Filename: "{app}\\bin\\WRIMS.bat"  \n' \
 						'Name:         "{app}\\@{Version} "; Filename: "{app}\\bin\\WRIMS.bat"  \n' \
 						'Name: "{userdesktop}\\@{Version} "; Filename: "{app}\\bin\\WRIMS.bat"; Tasks: desktopicon  \n' 
+
+XA_Automake_Template =  'MODULE=. \n'    \
+						'FILES=*.f90 \n' \
+						'COMPILE=@lf90 -nap -ndal -nchk -ntrace -inln -npca -nsav -nstchk -o3 -nw -nwo -c %fi -MOD %mo -ml msvc -dll -wisk -win \n' \
+						'LINK=@lf90 @%rf -exe %ex -ml msvc -mod %mo -dll -wisk -win -nomap -lib ..\wrangler\wrangler.lib,@{Version_XA} \n' \
+						'TARGET=simsolver.dll \n'
+
+
+
 calsim_path    = os.path.split( __file__)[0]
 VersionFile_path    = os.path.join(calsim_path,"version.txt")
 VersionWrapperFile_path    = os.path.join(calsim_path,"version_wrapper.inc")
 setupScriptFile_path    = os.path.join(calsim_path,"setup_include.iss")
+XA_AutomakeFile_path    = os.path.join(calsim_path,"engine","Simsolver","auto.config")
 
 print calsim_path
 
@@ -38,6 +49,7 @@ print calsim_path
 VersionFile = open(VersionFile_path, "w") 
 VersionWrapperFile = open(VersionWrapperFile_path, "w") 
 setupScriptFile = open(setupScriptFile_path, "w") 
+XA_AutomakeFile = open(XA_AutomakeFile_path, "w") 
 
 try:
 	(dummy, SVNVersion_SourceCode) = os.popen4("svnversion ..//calsim ")
@@ -54,6 +66,8 @@ try:
 	VersionWrapperTxt = VersionWrapperTxt.replace("@{Version}", Version_)
 	SetupScriptTxt = SetupScriptTxt.replace("@{Version}", Version_)
 
+	XA_Txt = XA_Automake_Template.replace("@{Version_XA}", Version_XA)
+	
 	VersionTxt = VersionTxt.replace("@{Version_Nospace}", Version_Nospace_)
 	VersionWrapperTxt = VersionWrapperTxt.replace("@{Version_Nospace}", Version_Nospace_)
 	SetupScriptTxt = SetupScriptTxt.replace("@{Version_Nospace}", Version_Nospace_)
@@ -61,16 +75,20 @@ try:
 	VersionFile.write(VersionTxt)
 	VersionWrapperFile.write(VersionWrapperTxt)
 	setupScriptFile.write(SetupScriptTxt)
+	XA_AutomakeFile.write(XA_Txt)
 	
 	VersionFile.close()
 	VersionWrapperFile.close()
 	setupScriptFile.close()
+	XA_AutomakeFile.close()
 
 except:
 	VersionFile.close()
 	VersionWrapperFile.close()
 	setupScriptFile.close()
+	XA_AutomakeFile.close()
 	os.remove(VersionFile_path) 
 	os.remove(VersionWrapperFile_path) 
 	os.remove(setupScriptFile_path) 
+	os.remove(XA_AutomakeFile) 
 	print 'Abort.... possible error in file /calsim/verion_generate.py' 
