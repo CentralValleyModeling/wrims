34439caf79ac693190e795a2ce039471157a09bf
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2010-10-21 19:02:52 +0000

    minor

--- wrims_v2/convert/compile.bat
+++ wrims_v2/convert/compile.bat
@@ -3,6 +3,8 @@ set path=D:\Java\jdk_x86\jdk1.6.0_21\bin;%path%
 javac -cp "libs\\antlr-3.2.jar" antlr-generated\\WRESL\\*.java
 javac -cp "libs\\testng-5.14.1.jar;libs\\antlr-3.2.jar;antlr-generated" src\\test\\*.java
 
+md bin
+
 md bin\\WRESL
 copy antlr-generated\\WRESL\\*.class bin\\WRESL\\*.class
 del  antlr-generated\\WRESL\\*.class
