1737e2bccd887efb7d0a05f3915cabcda88e7141
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-01-07 21:26:09 +0000

    fix windows compile batch

--- wrims_v2/convert/compile.bat
+++ wrims_v2/convert/compile.bat
@@ -1,6 +1,10 @@
-javac src\\evaluators\\*.java
+
 
 javac -cp "libs\\antlr-3.2.jar;src" antlr-generated\\wresl\\*.java
+
+
+javac -cp "libs\\testng-5.14.1.jar;libs\\antlr-3.2.jar;antlr-generated" src\\evaluators\\*.java
+
 
 javac -cp "libs\\testng-5.14.1.jar;libs\\antlr-3.2.jar;antlr-generated;src" src\\test\\convertWresl\\*.java
 
