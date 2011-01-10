6ac3c4d4cc0657de5cb97cb89924fa838331c1e9
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-01-10 16:15:32 +0000

    keep 1st version of design as a backup

---
+++ wrims_v2/parser_v1/clean.bat
@@ -0,0 +1,12 @@
+del antlr-generated\\*.tokens
+del antlr-generated\\Parser\\*.java
+del antlr-generated\\Parser\\*.class
+
+del src\\test\\*.class 
+del src\\Parser\\*.tokens 
+
+del bin\\test\\*.class
+del bin\\Parser\\*.class
+
+rmdir test-output /s /q
+
