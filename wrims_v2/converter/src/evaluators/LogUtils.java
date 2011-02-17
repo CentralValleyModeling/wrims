e58ddae714b11472e14c62dd02332ba0cb32399b
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-02-17 18:58:53 +0000

    use same output stream to prevent flushing time uncertainty.

--- wrims_v2/converter/src/evaluators/LogUtils.java
+++ wrims_v2/converter/src/evaluators/LogUtils.java
@@ -10,8 +10,6 @@ public class LogUtils {
 	public static void importantMsg(String msg){
 
 		System.out.println(msg);
-		
-		System.out.flush();
 	}
 	
 	public static void normalMsg(String msg){
@@ -19,60 +17,48 @@ public class LogUtils {
 		if (Parameters.printLevel>1){
 
 			System.out.println(msg);
-			
-			System.out.flush();
+
 		}
 	}
 
 	public static void errMsg(String msg){
 
-		System.err.println("# Error: "+msg);
+		 System.out.println("# Error: "+msg);
 		
-		System.err.flush();
-
 	}	
 	
 	public static void errMsg(String msg, String file){
 
-		System.err.println("# Error: "+msg+" @ "+file);
+		 System.out.println("# Error: "+msg+" @ "+file);
 		
-		System.err.flush();
+	}
+
+	public static void errMsg(String msg, String file1, String file2, Map<String, Set<String>> reverseMap) {
+
+		System.out.println("# Error: " + msg + " in files: ");
+
+		String sp = "  ";
+		System.out.println(sp + file1);
+		printTree(file1, reverseMap, sp);
+		System.out.println(sp + file2);
+		printTree(file2, reverseMap, sp);
 
 	}
 
-	public static void errMsg(String msg, String file1, String file2,  Map<String,Set<String>> reverseMap){
+	private static void printTree(String f, Map<String, Set<String>> reverseMap, String level) {
 
-		System.out.flush();
-		
-		System.err.println("# Error: "+msg+" in files: ");
-		String sp = "  ";
-		System.err.println(sp+file1);
-		printTree(file1,reverseMap, sp);		
-		System.err.println(sp+file2);
-		printTree(file2,reverseMap, sp);
-		
-		System.err.flush();
-
-
-	}	
-
-	private static void printTree(String f, Map<String,Set<String>> reverseMap, String level){
-
-		//String arrow = ">";
-		if(reverseMap.get(f)!=null){
+		// String arrow = ">";
+		if (reverseMap.get(f) != null) {
 			level = level + "--";
 			Set<String> parents = reverseMap.get(f);
-			for (String s : parents){
-				System.err.println(level+s);
-				printTree(s, reverseMap, level);
-				
-				
-			}
-			
-		}
-		
+			for (String s : parents) {
+				System.out.println(level + s);
 
-		
+				printTree(s, reverseMap, level);
+
+			}
+
+		}	
 
 
 	}	
