3380883e51e78ee77bd7f883099765f91e03fd67
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-03-11 16:38:00 +0000

    test relation statement

--- wrims_v2/wrims_v2/src/test/test_evaluator/TestEvaluator.java
+++ wrims_v2/wrims_v2/src/test/test_evaluator/TestEvaluator.java
@@ -12,15 +12,17 @@ import org.testng.annotations.Test;
 
 import wrimsv2.evaluator.EvaluatorLexer;
 import wrimsv2.evaluator.EvaluatorParser;
-import wrimsv2.components.*;
+import wrimsv2.components.Error;
+import wrimsv2.components.EvalConstraint;
+import wrimsv2.components.EvalExpression;
+import wrimsv2.components.MainFile;
 
 public class TestEvaluator {
 	
 	
 	public String inputFilePath;
 	public String logFilePath;
-
-	@Test	
+	
 	public void testRelationStatement() throws RecognitionException, IOException {
 		ANTLRStringStream stream = new ANTLRStringStream("g: (2+a)*3.4-(5+6*7.0)*b<(2+b)*6");
 		EvaluatorLexer lexer = new EvaluatorLexer(stream);
@@ -32,6 +34,26 @@ public class TestEvaluator {
 		System.out.println(ec.getSign());
 		System.out.println(ee.getValue());
 		System.out.println(ee.getMultiplier());
+	}
+	
+	@Test	
+	public void testConditionStatement() throws RecognitionException, IOException {
+        String mainFile="z:\\temp\\test";
+        new MainFile(mainFile);
+		
+		ANTLRStringStream stream = new ANTLRStringStream("c: (6+8)*4>(3-2) .or. 100-4<3");
+		EvaluatorLexer lexer = new EvaluatorLexer(stream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		EvaluatorParser evaluator = new EvaluatorParser(tokenStream);
+		evaluator.evaluator();
+		boolean eCondition=evaluator.evalCondition;
+		if (eCondition){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+		
+		Error.writeEvaluationErrorFile("log.txt");
 	}
 		
 }
