9eaa889f6f801acb4d163bcf5987f0a392c01d23
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-04-04 22:34:05 +0000

    related files for improve dll implementation

---
+++ wrims_v2/wrims_v2/src/test/test_external/processDll.bat
@@ -0,0 +1,10 @@
+javac -cp . wrimsv2\external\Functionann_x2.java
+javah -jni wrimsv2.external.Functionann_x2
+D:/cvwrsm/trunk/3rd_party/MinGW/bin/gcc -c -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include"  -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include/win32" wrimsv2_external_Functionann_x2.c
+javac -cp . wrimsv2\external\Functionannec.java
+javah -jni wrimsv2.external.Functionannec
+D:/cvwrsm/trunk/3rd_party/MinGW/bin/gcc -c -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include"  -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include/win32" wrimsv2_external_Functionannec.c
+javac -cp . wrimsv2\external\Functionannlinegen.java
+javah -jni wrimsv2.external.Functionannlinegen
+D:/cvwrsm/trunk/3rd_party/MinGW/bin/gcc -c -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include"  -I"C:/Program Files (x86)/Java/jdk1.6.0_24/include/win32" wrimsv2_external_Functionannlinegen.c
+D:/cvwrsm/trunk/3rd_party/MinGW/bin/gcc -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -shared wrimsv2_external_Functionannlinegen.o wrimsv2_external_Functionannec.o wrimsv2_external_Functionann_x2.o -o interfacetoann.dll -L./ Ann7inp_CA.dll
