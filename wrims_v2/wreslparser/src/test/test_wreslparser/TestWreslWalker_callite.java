76946747d5da7cf4b129efc657c6217fb618cbec
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-04-21 17:06:19 +0000

    add test for Hao's callite study

---
+++ wrims_v2/wreslparser/src/test/test_wreslparser/TestWreslWalker_callite.java
@@ -0,0 +1,53 @@
+
+package test.test_wreslparser;
+
+import java.io.File;
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import wrimsv2.commondata.wresldata.StudyDataSet;
+import wrimsv2.wreslparser.elements.LogUtils;
+import wrimsv2.wreslparser.elements.StudyConfig;
+import wrimsv2.wreslparser.elements.StudyParser;
+import wrimsv2.wreslparser.elements.TempData;
+import wrimsv2.wreslparser.elements.WriteCSV;
+
+public class TestWreslWalker_callite {
+	
+	public String projectPath = "src\\test\\test_wreslparser\\";	
+	public String inputFilePath;
+	public String logFilePath;	
+	public String csvFolderPath;	
+	
+	@Test(groups = { "WRESL_Callite" })
+	public void callite_hao() throws RecognitionException, IOException {
+		
+		csvFolderPath = "TestWreslWalker_callite_hao";
+		inputFilePath = "D:\\Haos_CalLite\\Run\\main.wresl";
+		logFilePath = csvFolderPath+".log";
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		
+		/// temporary dataset, don't use this because the structure will be changed soon. 
+		LogUtils.setLogFile(logFilePath);
+		TempData td = new TempData();
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		LogUtils.closeLogFile();
+		
+		
+		/// write to StudyDataSet
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+				
+		/// write full study data to csv files
+		WriteCSV.study(sd, csvFolderPath ) ;
+	
+		Assert.assertEquals(1, 0);	
+		
+	}	
+}
