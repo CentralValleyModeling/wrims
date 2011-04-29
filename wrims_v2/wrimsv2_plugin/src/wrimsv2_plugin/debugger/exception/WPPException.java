98ba83fc4e764b2ec10ad1e09fda32436c822ecb
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-04-29 15:39:27 +0000

    add exception view

---
+++ wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/debugger/exception/WPPException.java
@@ -0,0 +1,23 @@
+package wrimsv2_plugin.debugger.exception;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+import wrimsv2_plugin.debugger.view.WPPExceptionView;
+
+public class WPPException {
+	public static void handleException(final Exception e){
+		final IWorkbench workbench=PlatformUI.getWorkbench();
+		workbench.getDisplay().asyncExec(new Runnable(){
+			public void run(){
+				try {
+					WPPExceptionView exceptionView = (WPPExceptionView) workbench.getActiveWorkbenchWindow().getActivePage().showView("wpp.exceptionview");
+					exceptionView.addException(e);
+				} catch (PartInitException e) {
+					WPPException.handleException(e);
+				}
+			}
+		});
+	}
+}
