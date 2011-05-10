5b5c68189543e46577cfc6f31696247baf142854
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-05-10 16:20:56 +0000

    test simple study

---
+++ wrims_v2/wrims_v2/src/test/test_studies/testStudies.java
@@ -0,0 +1,178 @@
+package test.test_studies;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.antlr.runtime.RecognitionException;
+import org.testng.annotations.Test;
+
+import wrimsv2.commondata.wresldata.Goal;
+import wrimsv2.commondata.wresldata.ModelDataSet;
+import wrimsv2.commondata.wresldata.StudyDataSet;
+import wrimsv2.commondata.wresldata.Svar;
+import wrimsv2.components.ControlData;
+import wrimsv2.components.Controller;
+import wrimsv2.components.Error;
+import wrimsv2.components.FilePaths;
+import wrimsv2.wreslparser.elements.LogUtils;
+import wrimsv2.wreslparser.elements.StudyConfig;
+import wrimsv2.wreslparser.elements.StudyParser;
+import wrimsv2.wreslparser.elements.TempData;
+import wrimsv2.wreslparser.elements.WriteCSV;
+
+public class testStudies {
+
+	public void testSimpleStudy() throws RecognitionException, IOException{
+        FilePaths.fullSvarDssPath="Z:\\CalSimOrig\\CommonData\\BigExampleSV.dss";
+        FilePaths.fullInitDssPath="Z:\\CalSimOrig\\CommonData\\BigExampleINIT.dss";
+        FilePaths.setMainFilePaths("Z:\\CalSimOrig\\Example10\\run\\Example10.wresl");
+		ControlData cd=new ControlData();
+		cd.startYear=1921;
+		cd.startMonth=10;
+		cd.startDay=31;
+		cd.endYear=1921;
+		cd.endMonth=11;
+		cd.endDay=30;
+		cd.currYear=cd.startYear;
+		cd.currMonth=cd.startMonth;
+		cd.currDay=cd.startDay;
+		cd.svDvPartF="EXAMPLE";
+		cd.initPartF="INIT";
+		
+		StudyDataSet sds=parseSimpleStudy();
+		
+		new Controller(sds);
+		Error.writeEvaluationErrorFile("evaluation_error.txt");
+	}
+	
+	public StudyDataSet parseSimpleStudy() throws RecognitionException, IOException{
+		
+		String csvFolderPath = "TestWreslWalker_simple_study";
+		String inputFilePath = "Z:\\CalSimOrig\\Example10\\run\\Example10.wresl";
+		String logFilePath = csvFolderPath+".log";
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		
+		/// temporary dataset, don't use this because the structure will be changed soon. 
+		LogUtils.setLogFile(logFilePath);
+		TempData td = new TempData();
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		LogUtils.closeLogFile();
+		
+		
+		/// write to StudyDataSet
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+				
+		/// write full study data to csv files
+		WriteCSV.study(sd, csvFolderPath ) ;
+	
+		return sd;	
+	}
+	
+	public void testParsedCalsim3()throws RecognitionException, IOException{
+        FilePaths.fullSvarDssPath="D:\\CALSIM30_041311_BO\\common\\DSS\\CalSim30_06_SV.dss";
+        FilePaths.fullInitDssPath="D:\\CALSIM30_041311_BO\\common\\DSS\\CalSim30_06Init.dss";
+        FilePaths.setMainFilePaths("D:\\CALSIM30_041311_BO\\CONV\\Run\\mainCONV_30.wresl");
+		ControlData cd=new ControlData();
+		cd.startYear=1921;
+		cd.startMonth=10;
+		cd.startDay=31;
+		cd.endYear=2006;
+		cd.endMonth=11;
+		cd.endDay=30;
+		cd.currYear=cd.startYear;
+		cd.currMonth=cd.startMonth;
+		cd.currDay=cd.startDay;
+		cd.svDvPartF="CALSIM30_06";
+		cd.initPartF="CALSIM30_06";
+		
+		StudyDataSet sds=parseCalsim3();
+		
+		new Controller(sds);
+		Error.writeEvaluationErrorFile("evaluation_error.txt");
+	}
+	
+	public StudyDataSet parseCalsim3() throws RecognitionException, IOException{
+		
+		String csvFolderPath = "TestWreslWalker_calsim3_full_study";
+		String inputFilePath = "D:\\CALSIM30_041311_BO\\CONV\\Run\\mainCONV_30.wresl";
+		String logFilePath = csvFolderPath+".log";
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		
+		/// temporary dataset, don't use this because the structure will be changed soon. 
+		LogUtils.setLogFile(logFilePath);
+		TempData td = new TempData();
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		LogUtils.closeLogFile();
+		
+		
+		/// write to StudyDataSet
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+				
+		/// write full study data to csv files
+		WriteCSV.study(sd, csvFolderPath ) ;
+	
+		return sd;	
+	}
+
+	@Test
+	public void testParsedCalLite()throws RecognitionException, IOException{
+        FilePaths.fullSvarDssPath="D:\\CalLite_Beta_042611\\DSS\\CL_FUTURE_WHL042611_SV.dss";
+        FilePaths.fullInitDssPath="D:\\CalLite_Beta_042611\\DSS\\CalLite2020D09EINIT.dss";
+        FilePaths.setMainFilePaths("D:\\CalLite_Beta_042611\\Run\\main_BO.wresl");
+		ControlData cd=new ControlData();
+		cd.startYear=1921;
+		cd.startMonth=10;
+		cd.startDay=31;
+		cd.endYear=2003;
+		cd.endMonth=9;
+		cd.endDay=30;
+		cd.currYear=cd.startYear;
+		cd.currMonth=cd.startMonth;
+		cd.currDay=cd.startDay;
+		cd.svDvPartF="2020D09E";
+		cd.initPartF="2020D09E";
+		
+		StudyDataSet sds=parseCalLite();
+		
+		new Controller(sds);
+		Error.writeEvaluationErrorFile("evaluation_error.txt");
+	}
+	
+	public StudyDataSet parseCalLite() throws RecognitionException, IOException{
+		
+		String csvFolderPath = "TestWreslWalker_callite_full_study";
+		String inputFilePath = "D:\\CalLite_Beta_042611\\Run\\main_BO.wresl";
+		String logFilePath = csvFolderPath+".log";
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		
+		/// temporary dataset, don't use this because the structure will be changed soon. 
+		LogUtils.setLogFile(logFilePath);
+		TempData td = new TempData();
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		LogUtils.closeLogFile();
+		
+		
+		/// write to StudyDataSet
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+				
+		/// write full study data to csv files
+		WriteCSV.study(sd, csvFolderPath ) ;
+	
+		return sd;	
+	}
+}
