9acdb2e95dca84f32fce57a3d8e2511cdae23181
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-05-18 23:46:45 +0000

    private field is not safe with simple getter and setter.

---
+++ wrims_v2/wreslparser/src/test/test_wreslparser/TestWreslWalker_readonly.java
@@ -0,0 +1,27 @@
+
+package test.test_wreslparser;
+
+import java.util.ArrayList;
+
+public class TestWreslWalker_readonly {
+	
+	private ArrayList<String> x = new ArrayList<String>();
+	
+	TestWreslWalker_readonly(){
+		
+		x.add("original");
+	}
+
+	public ArrayList<String> getX() {
+		return x;
+	}
+
+	public void setX(ArrayList<String> x) {
+		this.x = x;
+	}	
+
+}
+
+
+	
+
