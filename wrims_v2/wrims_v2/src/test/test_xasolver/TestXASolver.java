397e9364b1424848555ee0dc13190fc1028b3ea8
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-06-15 22:52:00 +0000

    add test for example1 wresl study

---
+++ wrims_v2/wrims_v2/src/test/test_xasolver/TestXASolver.java
@@ -0,0 +1,40 @@
+package test.test_xasolver;
+
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import wrimsv2.components.*;
+
+public class TestXASolver {
+
+	@Test(groups = { "xa_solver_example_studies" })
+	public void example1() throws RecognitionException, IOException{
+		
+		/// set control data		
+		String[] controlDataString = {
+		"D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\example1\\run\\mainEx1.wresl",
+		"D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\example1\\dss\\ExampleSV.dss",
+		"D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\example1\\dss\\ExampleINIT.dss",
+		"",
+
+		"EXAMPLE",
+		"INIT",
+		"CALSIM",
+		"1MON",
+		"1921",
+		"10",
+		 "31",
+		 "1921",
+		 "11",
+		 "30", 
+		 "XA", 
+		 "csv_Example1"};
+		
+        new Controller(controlDataString);
+
+		// TODO: compare objective function value
+		Assert.assertEquals(0, 1);	
+	}
+}
