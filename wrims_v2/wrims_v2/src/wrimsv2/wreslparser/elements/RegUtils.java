782b2d1ddc768412fba46ae7c57f69bb537849b3
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-06-16 22:01:11 +0000

    parsing xa log file and use testNG to compare objective function value with accepted tolerance.

--- wrims_v2/wrims_v2/src/wrimsv2/wreslparser/elements/RegUtils.java
+++ wrims_v2/wrims_v2/src/wrimsv2/wreslparser/elements/RegUtils.java
@@ -4,7 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegUtils {
+	
+	
+	public static String getLastMatch(String inputStr, String patternStr) {
 
+		// String patternStr = "([a-zA-Z]+[0-9]+)";
+		String output = null;
+
+		int total = 0;
+		// Compile regular expression
+		Pattern pattern = Pattern.compile(patternStr);
+		Matcher matcher = pattern.matcher(inputStr);
+
+		// Count all occurrences of pattern in input
+		while (matcher.find()) {
+
+			total = total + 1;
+			output = matcher.group();
+		}
+		return output;
+	}
+
+	
+	
 
     public static String replaceWith(String inputStr, String patternStr, String replaceStr ) {
 
