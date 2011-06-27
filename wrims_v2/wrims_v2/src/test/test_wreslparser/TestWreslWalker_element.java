3b70fe60678ebeb94d32175a62285a6caafbdacc
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-06-27 21:53:08 +0000

    add weightTable test

--- wrims_v2/wrims_v2/src/test/test_wreslparser/TestWreslWalker_element.java
+++ wrims_v2/wrims_v2/src/test/test_wreslparser/TestWreslWalker_element.java
@@ -405,27 +405,73 @@ public class TestWreslWalker_element {
 		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
 		LogUtils.studySummary_details(sd);
 		LogUtils.closeLogFile();
-
-		String modelName = sd.getModelList().get(0);		
-		WriteCSV.dataset(sd.getModelDataSetMap().get(modelName),csvFolderPath ) ;
+		
+		WriteCSV.study(sd,csvFolderPath ) ;
 		String logText = Tools.readFileAsString(logFilePath);	
 
 		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
 		Assert.assertEquals(totalErrs, 0);	
 		
-		String csvText = Tools.readFileAsString(csvFolderPath+"\\weight.csv");	
+		String csvText = Tools.readFileAsString(csvFolderPath+"\\second\\weight.csv");	
 		
 		String s;
 		int n;
 	
-		s = "alias_global,flow-channel,cfs,C_SacFea+D_SacFea";
+		s = "errpos_shsta,-99999";
 		s = Tools.replace_regex(s);
 		n = RegUtils.timesOfMatches(csvText, s );
 		Assert.assertEquals(n, 1);
 		
-
+		csvText = Tools.readFileAsString(csvFolderPath+"\\third\\weight.csv");	
+	
+		s = "errpos_shsta,-99999";
+		s = Tools.replace_regex(s);
+		n = RegUtils.timesOfMatches(csvText, s );
+		Assert.assertEquals(n, 1);
 		
-		//Assert.assertEquals(sd.getModelDataSetMap().get(modelName).asList_global.get(0),"alias_global" );
-		//Assert.assertEquals(sd.getModelDataSetMap().get(modelName).asList_local.get(0),"alias_local" );
+		
+	}
+
+	@Test(groups = { "WRESL_elements" })
+	public void weightTable2() throws RecognitionException, IOException {
+		
+		csvFolderPath = "TestWreslWalker_element_weightTable2";
+		inputFilePath = projectPath+csvFolderPath+".wresl";
+		logFilePath = csvFolderPath+".log";
+	
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		TempData td = new TempData();
+	
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);		
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+		LogUtils.studySummary_details(sd);
+		LogUtils.closeLogFile();
+			
+		WriteCSV.study(sd,csvFolderPath ) ;
+		String logText = Tools.readFileAsString(logFilePath);	
+	
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		Assert.assertEquals(totalErrs, 0);	
+		
+		String csvText = Tools.readFileAsString(csvFolderPath+"\\second\\weight.csv");	
+		
+		String s;
+		int n;
+	
+		s = "errpos_shsta,-99999";
+		s = Tools.replace_regex(s);
+		n = RegUtils.timesOfMatches(csvText, s );
+		Assert.assertEquals(n, 0);
+
+		s = "x,20000";
+		s = Tools.replace_regex(s);
+		n = RegUtils.timesOfMatches(csvText, s );
+		Assert.assertEquals(n, 1);
+	
 	}
 }
