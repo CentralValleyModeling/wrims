f220a3c5ef19d30f21fd3ffacb7e25af5ce5ed20
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-06-30 22:26:38 +0000

    add python script to compare dvar in xa log

--- wrims_v2/wrims_v2/src/wrimsv2/tools/compare_dvar.py
+++ wrims_v2/wrims_v2/src/wrimsv2/tools/compare_dvar.py
@@ -5,24 +5,23 @@ compare_number_of_decimal = 6
 total_cycle = 2
 
 #### v1
-v1_label_local = ["01OBJECTIVE:","02OBJECTIVE:"]
-v1_label_global = "00OBJECTIVE:"
-v1_label_end = "= 0"
+v1_label_begin = "|"
+v1_label_end = "STATISTICS"
 v1_begin_line_offset = 0
-v1_end_line_offset = 0
+v1_end_line_offset = -2
 v1_infile_path  = "v1.log"
 
 #### v2
-v2_label_begin = "Maximize Solve Number"
-v2_label_end = "Constraints"
-v2_begin_line_offset = 1
+v2_label_begin = "|"
+v2_label_end = "STATISTICS"
+v2_begin_line_offset = 0
 v2_end_line_offset = -2
 v2_infile_path  = "v2.log"
 
 ################# more settings
-v1_outfile_prepend = "v1_objective"
-v2_outfile_prepend = "v2_objective"
-
+v1_outfile_prepend = "v1_dvar"
+v2_outfile_prepend = "v2_dvar"
+max_integer_length = compare_number_of_decimal + 7
 ################# end configuration ##############
 
 
@@ -37,7 +36,7 @@ def findObjLineRange(label_begin,label_end,begin_offset,end_offset,infile_path):
 
 	for i, line in enumerate(infile.readlines()):
 
-		if ( in_block_obj == False and (label_begin in line) ):
+		if ( (in_block_obj == False) and (label_begin in line) ):
 			i_start.append(i+begin_offset)
 			in_block_obj = True
 			
@@ -57,29 +56,23 @@ def findObjLineRange(label_begin,label_end,begin_offset,end_offset,infile_path):
 	return outPosDict	
 
 def step1(i_start,i_end,label,label_end,infile_path):
-## remove "+"; 
-## remove space between "-" and number
-## remove label
- 
+
 	infile = open(infile_path,"r")
 	#outfile = open("V1_obj_step1.log","w")
 	outString =""
 	for i, line in enumerate(infile.readlines()):
 		if (i>= i_start and i<= i_end):
 			
-			line=line.replace(label, "")
-			line=line.replace(label_end,"")
-			line=line.replace("- ", "-")
-			line=line.replace("+", " ")
+			line=line.replace("= FREE", "FREE")
 			#outfile.write(line)
 			outString=outString+line
 
 	#outfile.close()
 	infile.close()
+	#print "String: "+ outString
 	return outString
 
 def step2(inString):
-## correct id with weight==1 and correct minus sign
 	
 	outDict = {}
 	data = []
@@ -89,54 +82,61 @@ def step2(inString):
 	
 	for line in infile.split('\n'):
 		#line = line.replace("= 0", "")
-		line = line.strip().split()
-		for item in line:
+		for item in line.split('|'):
+			item= item.strip().split()
+			#print "item: "+str(item)
 			data.append(item)
 
 	outID=[]
-	outWeight=[]
-	findID = False
+	outMax=[]
+	outMin=[]
+	outIsFree=[]
 	
 	for item in data:
 		
-		if findID:
+		if len(item)== 5:
+			outID.append(item[2])
+			min = float(item[0])
+			max = float(item[4])
+			outMin.append(min)
+			outMax.append(max)
+			outIsFree.append(False)
+			
+		elif len(item)== 3:
+
 			try:
-				float(item)
-				print "Error! two consecutive numbers: "+item
-			
-			except:
-				outID.append(item)
-				findID = False
+				float(item[0])
+				outID.append(item[2])
+				min = float(item[0])
+				outMin.append(min)
+				outMax.append("NA")
 				
-		else: #find weight
-			try: 
-				weight = float(item)
-				outWeight.append(weight)	
-				findID = True
-		
 			except:
-				outWeight.append(1)
-				outID.append(item)
-				findID = False
+				outID.append(item[0])
+				outMin.append("NA")
+				max = float(item[2])
+				outMax.append(max)
 				
-	for id, weight in zip(outID, outWeight):
-		
-		isInteger = False
-		
-		# use integer format when possible
-		int_weight = int(weight)
-		if weight == int_weight:
-			weight = int_weight
-			isInteger = True
-		else:
-			isInteger = False
-		
-		# correct minus sign
-		if id[0]=="-":
-			id=id[1:]
-			weight = -weight		
+			finally:
+				outIsFree.append(False)
+
+				
+		elif len(item)==2:
 			
-		outDict[id]=[weight, isInteger]	
+			outID.append(item[0])
+			outMin.append("NA")
+			outMax.append("NA")
+			outIsFree.append(True)
+			
+		elif len(item)>0:
+			
+			print "#Error: "+str(item)	
+				
+	for id, min, max, isFree in zip(outID, outMin, outMax, outIsFree):
+		
+
+		#print id, min, max, isFree
+		outDict[id]=[min,max,isFree]	
 		#outfile.writelines(id+' '+str(weight)+'\n')	
 
 	#outfile.close()
@@ -150,33 +150,52 @@ def step3(inDict,outfile_path):
 	
 	for k,v in sorted(inDict.items()):
 		
-		isInteger = v[1]
+		isFree = v[2]
+		rangeStr = ""
 		
-		if isInteger:
-			numberStr = "{: 12g}".format(v[0])
+		if isFree:
+			rangeStr =  " Free"
+			#rangeStr = "{: 12g}".format(v[0])
 			
 		else:
-			sss = "{: "+str(compare_number_of_decimal)+"."+str(compare_number_of_decimal-1)+"e}"
-			numberStr = sss.format(v[0])
 			
-		e = "{0:32}".format(k) + numberStr
+			if v[0]!="NA":
+				
+				if v[0] == int(v[0]):
+					sss = "  Min"+"{:> "+str(max_integer_length)+"g}"
+					rangeStr = sss.format(v[0])
+				else:
+					sss = "{:> "+str(compare_number_of_decimal)+"."+str(compare_number_of_decimal-1)+"e}"
+					rangeStr = "  Min "+sss.format(v[0])
+					
+			else:	
+				sss = "{:>"+str(max_integer_length+5)+"}"
+				rangeStr = sss.format(" ")
+			
+				
+			if v[1]!="NA":
+				
+				if v[1] == int(v[1]):
+					sss = "  Max"+"{:> "+str(max_integer_length)+"g}"
+					rangeStr += sss.format(v[1])
+				else:
+					sss = "{:> "+str(compare_number_of_decimal)+"."+str(compare_number_of_decimal-1)+"e}"
+					rangeStr += "  Max "+sss.format(v[1])				
+
+						
+		e = "{0:32}".format(k) + rangeStr
 		
 		outfile.writelines(e+'\n')	
 	outfile.close()	
 
-def wrims1(label_global,label_local,label_end,begin_offset,end_offset,infile_path,outfile_prepend):
+def wrims1(label_begin,label_end,begin_offset,end_offset,infile_path,outfile_prepend):
 	
 	out_full_map = {}	
 	
 	### find line number		
-	posDict_global = findObjLineRange(label_global,label_end,begin_offset,end_offset,infile_path)
-	posDict_local ={}
-	
-	for icycle, label in enumerate(label_local):
-		posDict_local[icycle] = findObjLineRange(label,label_end,begin_offset,end_offset,infile_path)[0]
-	
+	posDict_global = findObjLineRange(label_begin,label_end,begin_offset,end_offset,infile_path)
+		
 	print posDict_global
-	print posDict_local
 	
 	###
 	
@@ -186,31 +205,16 @@ def wrims1(label_global,label_local,label_end,begin_offset,end_offset,infile_pat
 		term_global = "OBJ00"
 		term_local  = "OBJ" + "{:02d}".format(icycle+1)
 		
-		print term_local
-		
 		### begin parsing global obj
-		outStep1=step1(posDict_global[icycle]["start"],posDict_global[icycle]["end"],label_global,label_end,infile_path)
-		map_global=step2(outStep1)
-		
-		### remove OBJ00 
-		if map_global[term_global][0] == -1:
-			del map_global[term_global]
-		else:
-			print "Error global term."
-			print map_global[term_global]		
+		outStep1=step1(posDict_global[icycle]["start"],posDict_global[icycle]["end"],label_begin,label_end,infile_path)
+		map=step2(outStep1)
 				
-		### begin parsing local obj
-		outStep1=step1(posDict_local[icycle]["start"],posDict_local[icycle]["end"],label_local[icycle],label_end,infile_path)
-		map_local=step2(outStep1)
+		### remove OBJ00 
+		del map[term_global]
+		del map[term_local]
 		
-		### remove OBJ0X 
-		if map_local[term_local][0] == -1:
-			del map_local[term_local]
-		else:
-			print "Error local term."
-			print map_local[term_local]
 					
-		merged_map = dict(map_global.items() + map_local.items())
+		merged_map = dict(map.items())
 		
 		# final output
 		out_full_map[icycle] = merged_map
@@ -235,17 +239,7 @@ def wrims2(label_start,label_end,begin_offset,end_offset,infile_path,outfile_pre
 		
 		### begin parsing global obj
 		outStep1=step1(posDict_global[icycle]["start"],posDict_global[icycle]["end"],label_start,label_end,infile_path)
-		map=step2(outStep1)	
-
-		### remove "OBJ:"
-		term_global = "OBJ:" 
-		
-		if map[term_global][0] == 1:
-			del map[term_global]
-		else:
-			print "Error global term."
-			print map[term_global]					
-
+		map=step2(outStep1)				
 					
 		merged_map = map
 		
@@ -258,6 +252,6 @@ def wrims2(label_start,label_end,begin_offset,end_offset,infile_path,outfile_pre
 	return out_full_map
 		
 ### call functions
-v1_map = wrims1(v1_label_global,v1_label_local,v1_label_end,v1_begin_line_offset,v1_end_line_offset,v1_infile_path,v1_outfile_prepend)
+v1_map = wrims1(v1_label_begin,v1_label_end,v1_begin_line_offset,v1_end_line_offset,v1_infile_path,v1_outfile_prepend)
 v2_map = wrims2(v2_label_begin,v2_label_end,v2_begin_line_offset,v2_end_line_offset,v2_infile_path,v2_outfile_prepend)
 	
