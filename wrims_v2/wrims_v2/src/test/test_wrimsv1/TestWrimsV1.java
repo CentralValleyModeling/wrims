661d678e598b7b4b9480d4d9f7729de6015fd0bd
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-08-12 23:55:11 +0000

    add wrims_v1 tests

---
+++ wrims_v2/wrims_v2/src/test/test_wrimsv1/TestWrimsV1.java
@@ -0,0 +1,135 @@
+
+package test.test_wrimsv1;
+
+import java.io.File;
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import wrimsv2.commondata.wresldata.StudyDataSet;
+import wrimsv2.wreslparser.elements.LogUtils;
+import wrimsv2.wreslparser.elements.RegUtils;
+import wrimsv2.wreslparser.elements.StudyConfig;
+import wrimsv2.wreslparser.elements.StudyParser;
+import wrimsv2.wreslparser.elements.TempData;
+import wrimsv2.wreslparser.elements.Tools;
+import wrimsv2.wreslparser.elements.WriteCSV;
+
+public class TestWrimsV1 {
+	
+	public String projectPath = "src\\test\\test_wrims1\\wresl_files\\";	
+	public String inputFilePath;
+	public String logFilePath;	
+	public String csvFolderPath;
+	public String testName;	
+	
+	@Test(groups = { "WRIMS_V1" })
+	public void case1() throws RecognitionException, IOException {
+		
+		testName = "TestWrimsV1_case1";
+		csvFolderPath = "testResult\\"+testName;
+		inputFilePath = projectPath + testName+".wresl";
+		logFilePath = csvFolderPath+".log";
+
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		TempData td = new TempData();
+
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+
+		LogUtils.studySummary_details(sd);
+
+		LogUtils.closeLogFile();
+		
+		String modelName = sd.getModelList().get(0);
+		
+		WriteCSV.dataset(sd.getModelDataSetMap().get(modelName),csvFolderPath ) ;
+	
+		String logText = Tools.readFileAsString(logFilePath);	
+
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		Assert.assertEquals(totalErrs, 6);	
+
+	}
+
+	@Test(groups = { "WRIMS_V1" })
+	public void case2() throws RecognitionException, IOException {
+		
+		testName = "TestWrimsV1_case2";
+		csvFolderPath = "testResult\\"+testName;
+		inputFilePath = projectPath + testName+".wresl";
+		logFilePath = csvFolderPath+".log";
+	
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		TempData td = new TempData();
+	
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+	
+		LogUtils.studySummary_details(sd);
+	
+		LogUtils.closeLogFile();
+		
+		String modelName = sd.getModelList().get(0);
+		
+		WriteCSV.dataset(sd.getModelDataSetMap().get(modelName),csvFolderPath ) ;
+	
+		String logText = Tools.readFileAsString(logFilePath);	
+	
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		Assert.assertEquals(totalErrs, 6);	
+	
+	}
+
+	@Test(groups = { "WRIMS_V1" })
+	public void case3() throws RecognitionException, IOException {
+		
+		testName = "TestWrimsV1_case3";
+		csvFolderPath = "testResult\\"+testName;
+		inputFilePath = projectPath + testName+".wresl";
+		logFilePath = csvFolderPath+".log";
+	
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		TempData td = new TempData();
+	
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+	
+		LogUtils.studySummary_details(sd);
+	
+		LogUtils.closeLogFile();
+		
+		String modelName = sd.getModelList().get(0);
+		
+		WriteCSV.dataset(sd.getModelDataSetMap().get(modelName),csvFolderPath ) ;
+	
+		String logText = Tools.readFileAsString(logFilePath);	
+	
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		Assert.assertEquals(totalErrs, 6);	
+	
+	}
+}
