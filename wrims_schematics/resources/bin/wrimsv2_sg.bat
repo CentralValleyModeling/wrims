318ff3be2045f8f37e90c5c4c344c84b4f60ca57
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-09-06 21:58:50 +0000

    1) include class files to jar 2) add a wrimsv2_SG.bat file to resource\bin

---
+++ wrims_schematics/resources/bin/wrimsv2_sg.bat
@@ -0,0 +1,78 @@
+@echo off
+rem ###################################
+rem Batch file for running wrims schematic
+rem ###################################
+setlocal
+set wrims_schematic_home=%~dp0/..
+set Java_Bin=%~dp0/../jre6/bin/
+set WRIMSv2_Engine_Home=%~dp0/../
+set Vista_Lib=%~dp0/../vista/lib
+rem echo %wrims_schematic_home%
+if exist "%wrims_schematic_home%/lib/wrimsv2_SG.jar" goto :valid
+
+
+:notfound
+
+echo ############################################################
+echo   Error: WRIMS Schematic files not found
+echo   ___
+echo   Installation instructions
+echo   ___
+echo   The value of the environment variable wrims_schematic_home in the 
+echo   file wrims_schematic.bat needs to match the location where
+echo   WRIMS Schematic has been installed
+echo ############################################################
+PAUSE
+goto :end
+
+:valid
+rem ###############
+rem Set path to location of dll
+rem ###############
+set JNILIB=%wrims_schematic_home%/lib
+set JARDIR=%wrims_schematic_home%/lib
+set SYSJARDIR=%wrims_schematic_home%/lib/sys
+set path=%wrims_schematic_home%/lib;%path%
+:-------------:
+: system jars :
+:-------------:       
+set SYSJARS=%SYSJARDIR%\codebase.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jython.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jythonlib.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jythonUtils.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jxl.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jai_codec.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jai_imageio.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jai_core.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jcommon.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jfreechart.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jh.jar;%SYSJARS%
+set SYSJARS=%SYSJARDIR%\jdom.jar;%SYSJARS%
+:------------------:
+: application jars :
+:------------------:
+set APPJARS=%JARDIR%\heclib.jar
+set APPJARS=%JARDIR%\images.jar;%APPJARS%
+set APPJARS=%JARDIR%\rma.jar;%APPJARS%
+set APPJARS=%JARDIR%\hecdata.jar;%APPJARS%
+set APPJARS=%JARDIR%\gridUtil.jar;%APPJARS%
+set APPJARS=%JARDIR%\hec.jar;%APPJARS%
+set APPJARS=%JARDIR%\help\dssvueHelp.jar;%APPJARS%
+set APPJARS=%Vista_Lib%\vista.jar;%APPJARS%
+:-----------------:
+: schematic jars  :
+:-----------------:
+set SCHJARS=%JARDIR%\JGo.jar;%JARDIR%\JGoSVG.jar;%JARDIR%\wrimsv2_SG.jar
+:------------:
+: class path :
+:------------:
+set CLASSPATH=-classpath %SCHJARS%;%APPJARS%;%SYSJARS%
+
+rem ###############
+rem starting wrims schematic
+rem ###############
+:start
+"%wrims_schematic_home%/jre6/bin/java" -mx512m  -Djava.library.path="%JNILIB%" %CLASSPATH%  wrims.schematic.Schematic %1%
+:end
+endlocal 
+rem 
