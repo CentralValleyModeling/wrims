4eeae2bad3ae27f6d679487a96be95bb7ce546ce
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-09-09 15:46:58 +0000

    clean wrapper directory

--- wrims_schematics/dllwrapper/wrapper.bat
+++ wrims_schematics/dllwrapper/wrapper.bat
@@ -5,5 +5,14 @@ set wrims_home=%~dp0/..
 set JNILIB=%wrims_home%/lib
 
 "%wrims_home%/jre6/bin/java" -Djava.library.path="%JNILIB%" -cp "%JNILIB%/WRIMSv2.jar" wrimsv2.external.GenerateCompileFiles "%control_file%" %~dp0
-processDll
+start /w processDll
 
+copy wrimsv2\external\*.class ..\lib\wrimsv2\external
+rd /s wrimsv2
+del wrimsv2
+
+del *.o
+del *.h
+del *.c
+del error.log
+del processDll.bat
