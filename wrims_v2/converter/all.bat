1ced89288eae58b0bfcb2c1043ed71909a1e8b39
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-09-29 16:15:19 +0000

    delete obsolete files

--- wrims_v2/converter/all.bat
+++
@@ -1,5 +0,0 @@
-call clean.bat
-call localSetting.bat
-call gen_antlr.bat
-call compile.bat
-call test.bat
--- wrims_v2/converter/gen_antlr.bat
+++
@@ -1,3 +0,0 @@
-cd src
-java -jar ..\\libs\\antlr-3.2.jar -o ..\\antlr-generated wresl\\*.g
-cd ..
