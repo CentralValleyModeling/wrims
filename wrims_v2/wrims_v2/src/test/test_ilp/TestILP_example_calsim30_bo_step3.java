8ff1dce43706b128b26b457e67a19d227c82dd56
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-10-24 22:03:44 +0000

    minor

---
+++ wrims_v2/wrims_v2/src/test/test_ilp/TestILP_example_calsim30_bo_step3.java
@@ -0,0 +1,61 @@
+package test.test_ilp;
+
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import wrimsv2.components.*;
+import wrimsv2.wreslparser.elements.RegUtils;
+import wrimsv2.wreslparser.elements.Tools;
+
+public class TestILP_example_calsim30_bo_step3 {
+	
+	private double tolerance_perc = 0.1/1000000; // 0.1 ppm
+	private String studyPath;	
+	private String dssPath;	
+	private double expected;
+	
+	@Test(groups = { "ilp_example_calsim3" })
+	public void calsim3_step3() throws RecognitionException, IOException{
+		
+		studyPath = "D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\calsim30_bo_svn51\\";
+		dssPath = "D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\calsim30_bo_svn51\\common\\DSS\\";
+		
+		/// set control data		
+		String[] controlDataString = {
+		studyPath + "common\\CVGroundwater\\Data\\",   //groundwater dir
+		studyPath + "CONV\\run\\mainCONV_30.wresl",
+		dssPath +   "CalSim30_06_SV.dss",
+		dssPath +   "CalSim30_06Init.dss",
+		studyPath + "CONV\\DSS\\dv.dss",
+		"CalSim30_06",       // sv dv F part
+		"CalSim30_06",   // init file F part
+		"CALSIM",        // part A
+		"1MON",
+		"1921",
+		"10",
+		 "31",
+		 "1921",
+		 "12",
+		 "31", 
+		 "ILP", 
+		 "csv"};
+
+		FilePaths.ilpFileDirectory = "ilp_TestIlp_example_calsim30_bo_step3";
+		//FilePaths.ilpFile = "test.ilp";
+		
+        new Controller(controlDataString);
+
+        
+        expected = 1.643754258492466E10; 
+                                            
+		
+		double obj_value =  ControlData.xasolver.getObjective();
+		
+		Assert.assertEquals(obj_value, expected, expected*tolerance_perc);	
+
+	}
+	
+
+}
