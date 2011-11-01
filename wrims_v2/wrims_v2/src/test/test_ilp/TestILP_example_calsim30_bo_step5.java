6856a36e20b3f43de44cd5d4cb1c2bad438eba60
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-11-01 21:20:14 +0000

    minor

--- wrims_v2/wrims_v2/src/test/test_ilp/TestILP_example_calsim30_bo_step5.java
+++ wrims_v2/wrims_v2/src/test/test_ilp/TestILP_example_calsim30_bo_step5.java
@@ -48,7 +48,7 @@ public class TestILP_example_calsim30_bo_step5 {
         new Controller(controlDataString);
 
         
-        expected = 1.5022882014598475E10; 
+        expected = 1.5022887623455383E10; 
                                             
 		
 		double obj_value =  ControlData.xasolver.getObjective();
