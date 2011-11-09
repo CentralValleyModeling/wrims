ceeaa5367f286ba252bb703d6ad9ae2e145872e1
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-11-09 21:36:33 +0000

    minor fix in test.

--- wrims_v2/wrims_v2/src/wrimsv2/external/Functioninitgwsystem.java
+++ wrims_v2/wrims_v2/src/wrimsv2/external/Functioninitgwsystem.java
@@ -14,19 +14,21 @@ public class Functioninitgwsystem extends ExternalFunction{
 
 		//values in reverse order:
 		Object param3 = stack.pop();
-		Object param2 = stack.pop();
-		Object param1 = stack.pop();
+		//Object param2 = stack.pop();
+		//Object param1 = stack.pop();
 
 		//cast params to correct types:
 		int iWRIMS1Run = ((Number) param3).intValue();
-		int Month = ((Number) param2).intValue();
-		int WaterYear = ((Number) param1).intValue();
+		//int Month = ((Number) param2).intValue();
+		//int WaterYear = ((Number) param1).intValue();
 
-		float result = initgwsystem(WaterYear, Month, iWRIMS1Run);
+		//float result = initgwsystem(WaterYear, Month, iWRIMS1Run);
+		float result = initgwsystem(iWRIMS1Run);
 
 		// push the result on the Stack
 		stack.push(new Float(result));
 	}
 
-	public native float initgwsystem(int WaterYear, int Month, int iWRIMS1Run);
+	//public native float initgwsystem(int WaterYear, int Month, int iWRIMS1Run);
+	public native float initgwsystem(int iWRIMS1Run);
 }
