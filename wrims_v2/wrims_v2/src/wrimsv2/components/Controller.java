fa189896d3094b8fad0337277bbbc273457bdcea
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-11-21 18:22:30 +0000

    add license key for CalLite

--- wrims_v2/wrims_v2/src/wrimsv2/components/Controller.java
+++ wrims_v2/wrims_v2/src/wrimsv2/components/Controller.java
@@ -118,6 +118,7 @@ public class Controller {
 		cd.endDay=30;
         cd.solverName="XA";
         FilePaths.csvFolderName="csv";
+        
 		cd.currYear=cd.startYear;
 		cd.currMonth=cd.startMonth;
 		cd.currDay=cd.startDay;
@@ -523,7 +524,8 @@ public class Controller {
 		ControlData.xasolver=new Optimizer(25000);
 		ControlData.xasolver.openConnection();
 		ControlData.xasolver.setModelSize(100, 100);
-		ControlData.xasolver.setCommand("MAXIMIZE Yes MUTE yes FORCE No wait no matlist v set visible no"); 
+		ControlData.xasolver.setCommand("MAXIMIZE Yes MUTE yes FORCE No wait no matlist v set visible no");
+		ControlData.xasolver.setActivationCodes( 234416483 , 19834525 ) ;
 		//ControlData.xasolver.setCommand("set sortName Yes FileName d:\\temp Output v2%d.log MatList V MPSX Yes ToRcc Yes");    //rcc code
 		//ControlData.xasolver.setCommand( "FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName Yes MatList V MPSX Yes ToRcc Yes set debug Yes  ListInput Yes")    //xa debug ;
 		if (ControlData.solverName.equalsIgnoreCase("XALOG") ) ControlData.xasolver.setCommand( "FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName W MatList V MPSX Yes ToRcc Yes") ;
