e63fb6f08c320b13d4b90ac3bc3b762047fd049a
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-11-21 18:44:25 +0000

    change sequence of initial xa

--- wrims_v2/wrims_v2/src/wrimsv2/components/ControllerDebug.java
+++ wrims_v2/wrims_v2/src/wrimsv2/components/ControllerDebug.java
@@ -541,10 +541,10 @@ public class ControllerDebug extends Thread {
 	
 	public void initialXASolver(){
 		ControlData.xasolver=new Optimizer(25000);
+		ControlData.xasolver.setActivationCodes( 234416483 , 19834525 ) ;
 		ControlData.xasolver.openConnection();
 		ControlData.xasolver.setModelSize(100, 100);
 		ControlData.xasolver.setCommand("MAXIMIZE Yes MUTE yes FORCE No wait no matlist v set visible no");
-		ControlData.xasolver.setActivationCodes( 234416483 , 19834525 ) ;
 		//ControlData.xasolver.setCommand("set sortName Yes FileName d:\\temp Output v2%d.log MatList V MPSX Yes ToRcc Yes");    //rcc code
 		//ControlData.xasolver.setCommand( "FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName Yes MatList V MPSX Yes ToRcc Yes set debug Yes  ListInput Yes")    //xa debug ;
 		if (ControlData.solverName.equalsIgnoreCase("XALOG") ) ControlData.xasolver.setCommand( "FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName W MatList V MPSX Yes ToRcc Yes") ;
