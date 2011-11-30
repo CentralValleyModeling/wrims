7dbc949c15a3952cc1a6c034e977abb2643f4631
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2011-11-30 23:17:15 +0000

    update Tools.readFileAsString

--- wrims_v2/wrims_v2/src/wrimsv2/wreslparser/elements/Tools.java
+++ wrims_v2/wrims_v2/src/wrimsv2/wreslparser/elements/Tools.java
@@ -8,7 +8,10 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -106,6 +109,31 @@ public class Tools {
 				}
 		}
 		return new String(buffer);
+	}
+	
+
+	public static String readFileAsString(String file, String csName) throws IOException {
+		Charset cs = Charset.forName(csName);
+		// Thanks to Jon Skeet
+		// No real need to close the BufferedReader/InputStreamReader
+		// as they're only wrapping the stream
+		FileInputStream stream = new FileInputStream(file);
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+			StringBuilder builder = new StringBuilder();
+			char[] buffer = new char[8192];
+			int read;
+			while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			return builder.toString();
+		}
+		finally {
+			// Potential issue here: if this throws an IOException,
+			// it will mask any others. Normally I'd use a utility
+			// method which would log exceptions and swallow them
+			stream.close();
+		}
 	}
 
 	public static String readFileLine(String filePath) throws IOException {
