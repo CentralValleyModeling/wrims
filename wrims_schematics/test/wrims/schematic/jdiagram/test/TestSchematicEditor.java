497f39c8d043ca7d0142db4a58840feb6040ecad
water\psandhu <water\psandhu@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-01-10 20:58:54 +0000

    added schematic editor improved ability to compare mixed schematics of say Calsim 2 with Calsim 3

---
+++ wrims_schematics/test/wrims/schematic/jdiagram/test/TestSchematicEditor.java
@@ -0,0 +1,26 @@
+package wrims.schematic.jdiagram.test;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+
+import wrims.schematic.jdiagram.SchematicEditor;
+import wrims.schematic.jdiagram.SchematicEditorFrame;
+
+import com.mindfusion.diagramming.ShapeLibrary;
+import com.mindfusion.diagramming.ShapeListBox;
+
+public class TestSchematicEditor {
+
+	public static void main(String[] args) throws Exception{
+		new SchematicEditorFrame();
+	}
+}
