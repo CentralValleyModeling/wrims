41c53555e97cf6bdb75e5c5b30d827abbeacc82c
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-03-02 17:47:50 +0000

    support mod keyword

---
+++ wrims_v2/wrims_v2/src/test/test_wreslparser/TestWreslWalker_intrinsic.java
@@ -0,0 +1,74 @@
+
+package test.test_wreslparser;
+
+import java.io.File;
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import wrimsv2.commondata.wresldata.StudyDataSet;
+import wrimsv2.wreslparser.elements.LogUtils;
+import wrimsv2.wreslparser.elements.RegUtils;
+import wrimsv2.wreslparser.elements.StudyConfig;
+import wrimsv2.wreslparser.elements.StudyParser;
+import wrimsv2.wreslparser.elements.TempData;
+import wrimsv2.wreslparser.elements.Tools;
+import wrimsv2.wreslparser.elements.WriteCSV;
+
+public class TestWreslWalker_intrinsic {
+	
+	public String projectPath = "src\\test\\test_wreslparser\\wresl_files\\";	
+	public String inputFilePath;
+	public String logFilePath;	
+	public String csvFolderPath;
+	public String testName;	
+	
+	@Test(groups = { "WRESL_elements" })
+	public void modulus() throws RecognitionException, IOException {
+		
+		testName = "TestWreslWalker_intrinsic_mod";
+		csvFolderPath = "testResult_v1\\"+testName;
+		inputFilePath = projectPath + testName+".wresl";
+		logFilePath = csvFolderPath+".log";
+
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		TempData td = new TempData();
+
+		StudyConfig sc = StudyParser.processMainFileIntoStudyConfig(absFilePath);
+		
+		td.model_dataset_map=StudyParser.parseModels(sc,td);
+		
+		StudyDataSet sd = StudyParser.writeWreslData(sc, td); 
+
+		LogUtils.closeLogFile();
+		
+		String modelName = sd.getModelList().get(0);
+		
+		WriteCSV.study(sd,csvFolderPath ) ;
+		
+		String logText = Tools.readFileAsString(logFilePath);	
+
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		Assert.assertEquals(totalErrs, 0);	
+		
+	
+		String csvText = Tools.readFileAsString(csvFolderPath+"\\"+modelName+"\\svar.csv");	
+		
+		String s;
+		int n;
+	
+		s ="daysinprevmo,##,feb29,1,month==mar .and. mod(wateryear;4)==0.,";
+		s = Tools.replace_regex(s);
+		n = RegUtils.timesOfMatches(csvText, s );
+		Assert.assertEquals(n, 1);
+
+		
+	}
+	
+}
--- wrims_v2/wrims_v2/src/wrimsv2/commondata/wresldata/Param.java
+++ wrims_v2/wrims_v2/src/wrimsv2/commondata/wresldata/Param.java
@@ -38,7 +38,7 @@ public class Param {
 				 "prevjan", "prevfeb", "prevmar", "prevapr", "prevmay", "prevjun", 
 				 "prevjul", "prevaug", "prevsep", "prevoct", "prevnov", "prevdec",
 				 "wateryear", "cfs_taf", "taf_cfs", "cfs_cfm", "af_cfs", "cfs_af", "daysin", "daysinmonth",
-				 "i", "null", "pow", "exp", "log", "log10", "abs", "real",
+				 "i", "null", "pow", "exp", "log", "log10", "abs", "real","mod",
 				 "day"
 				 ));
 	  
