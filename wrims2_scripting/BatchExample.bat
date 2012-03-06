7115329f85d2ffa9fdfe9c5b6892b1d1f7b6d477
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-03-06 20:02:17 +0000

    add simple example for batch run

---
+++ wrims2_scripting/BatchExample.bat
@@ -0,0 +1,3 @@
+
+start /wait RunStudy_CalLiteLicense.bat D:\cvwrsm\trunk\wrims2_scripting\studies\callite_svn47\CONV\Run\CONV_test1.conifg
+start /wait RunStudy_CalLiteLicense.bat D:\cvwrsm\trunk\wrims2_scripting\studies\callite_svn47\CONV\Run\CONV_test2.conifg
