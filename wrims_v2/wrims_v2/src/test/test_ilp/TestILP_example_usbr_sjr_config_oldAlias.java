49bd8c711783d8952e9e15de35a1a900ea999255
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-03-30 18:49:15 +0000

    update test

---
+++ wrims_v2/wrims_v2/src/test/test_ilp/TestILP_example_usbr_sjr_config_oldAlias.java
@@ -0,0 +1,41 @@
+package test.test_ilp;
+
+import java.io.IOException;
+import org.antlr.runtime.RecognitionException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import wrimsv2.components.ControlData;
+import wrimsv2.components.ControllerBatch;
+import wrimsv2.components.Error;
+import wrimsv2.components.FilePaths;
+
+public class TestILP_example_usbr_sjr_config_oldAlias {
+	
+	private double tolerance_perc = 0.0001/1000000; // 0.1 ppb
+	private String studyPath;	
+	private double expected;
+	
+	@Test(groups = { "ilp_config_example_calsim3_usbr_sjr" })
+	public void usbr_oldAlias_1step() throws RecognitionException, IOException{
+		
+		studyPath = "D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\usbr_sjr\\D1641\\";
+		/// set control data		
+		String[] controlDataString = {
+
+		"-config=\"D:\\cvwrsm\\trunk\\wrims_v2\\wrims_v2\\examples\\usbr_sjr\\D1641\\Run\\usbr_sjr_oldAlias.config\"" 
+
+		};
+
+		//FilePaths.ilpFileDirectory = studyPath + "ilp_TestILP_example_callite_config_Nsteps";
+		//FilePaths.ilpFile = "test.ilp";		
+		Error.clear();
+        new ControllerBatch(controlDataString);
+        
+        expected = 1.40024607177482E10; 
+		
+		double obj_value =  ControlData.xasolver.getObjective();		
+		Assert.assertEquals(Error.getTotalError(), 0);	
+		Assert.assertEquals(obj_value, expected, expected*tolerance_perc);	
+	}
+}
