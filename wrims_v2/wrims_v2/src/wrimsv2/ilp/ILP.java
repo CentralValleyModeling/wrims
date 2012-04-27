87c2485b3f15fad1d98a5bf2100ec394d8585acf
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-04-27 17:34:05 +0000

    comment out ampl format ouput

--- wrims_v2/wrims_v2/src/wrimsv2/ilp/ILP.java
+++ wrims_v2/wrims_v2/src/wrimsv2/ilp/ILP.java
@@ -3,6 +3,7 @@ package wrimsv2.ilp;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -20,6 +21,7 @@ import wrimsv2.components.ControlData;
 import wrimsv2.components.Error;
 import wrimsv2.components.FilePaths;
 import wrimsv2.evaluator.EvalConstraint;
+import wrimsv2.solver.LPSolveSolver;
 import wrimsv2.wreslparser.elements.StudyUtils;
 import wrimsv2.wreslparser.elements.Tools;
 
@@ -38,7 +40,8 @@ public class ILP {
 	private static Set<String> dvar_effective;
 	private static final String lpSolve_comment_Symbol = "//";
 	private static final String ampl_comment_Symbol = "#";
-
+	private static DecimalFormat df;
+	
 	private ILP() {
 		
 	}
@@ -46,47 +49,53 @@ public class ILP {
 	public static void initializeIlp() {
 		
 		setLpSolveParentDir();
-		setAmplParentDir();
+		//setAmplParentDir();
 		// TODO: write ampl command file "option presolve_eps 1e-13;"
+		
+		
+		//TODO: move to somewhere else
+		df = new DecimalFormat();
+		//df.setMinimumFractionDigits(2);
+		df.setMaximumFractionDigits(8);
 	}
 	
 	public static void setIlpFile() {
 		
 		setLpSolveFile();
-		setAmplFile();		
+		//setAmplFile();		
 	}
 
 	public static void writeIlp() {
 		
 		writeLpSolveFile();
-		writeAmplFile();
+		//writeAmplFile();
 		
 		_lpSolveFile.flush();
-		_amplFile.flush();
+		//_amplFile.flush();
 	}
 
 	public static void writeObjValue_XA() {
 		
 		double objValue = ControlData.xasolver.getObjective();
 		writeObjValue(Double.toString(objValue), _lpSolveFile, lpSolve_comment_Symbol);
-		writeObjValue(Double.toString(objValue), _amplFile, ampl_comment_Symbol);
+		//writeObjValue(Double.toString(objValue), _amplFile, ampl_comment_Symbol);
 	
 	}
 	
 	public static void writeObjValue_LPSOLVE() {
 		
 		double objValue;
-		try {
-			objValue = ControlData.lpssolver.getObjective();
+		//try {
+			objValue = ControlData.lpsolve_objective;
 			writeObjValue(Double.toString(objValue), _lpSolveFile, lpSolve_comment_Symbol);
-			writeObjValue(Double.toString(objValue), _amplFile, ampl_comment_Symbol);
-		}
-		catch (LpSolveException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			writeObjValue(" LpSolveException Error! ", _lpSolveFile, lpSolve_comment_Symbol);
-			writeObjValue(" LpSolveException Error! ", _amplFile, ampl_comment_Symbol);
-		}
+			//writeObjValue(Double.toString(objValue), _amplFile, ampl_comment_Symbol);
+		//}
+//		catch (LpSolveException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			writeObjValue(" LpSolveException Error! ", _lpSolveFile, lpSolve_comment_Symbol);
+//			writeObjValue(" LpSolveException Error! ", _amplFile, ampl_comment_Symbol);
+//		}
 
 	
 	}
@@ -135,19 +144,25 @@ public class ILP {
 	public static void writeDvarValue_XA() {
 		
 		writeDvarValue_XA(_dvarFile, dvar_effective);
+		_dvarFile.flush();
 		
 	}
-
+	public static void writeDvarValue_LPSOLVE() {
+		
+		writeDvarValue_LPSOLVE(_dvarFile, dvar_effective);
+		_dvarFile.flush();
+		
+	}
 	public static void writeSvarValue() {
 		
 		writeSvarValue(_svarFile);
-		
+		_svarFile.flush();
 	}
 	
 	public static void closeIlpFile() {
 	
 		_lpSolveFile.close();
-	    _amplFile.close();
+	    //_amplFile.close();
 		_svarFile.close();
 		_dvarFile.close();
 	}
@@ -357,13 +372,61 @@ public class ILP {
 		dvarFile.println("/* Weighted Dvar    */");
 		for (String s : dvar_weighted){
 			String dvName = String.format("%-35s", s);
-			dvarFile.print(dvName + ":  " + ControlData.xasolver.getColumnActivity(s) +"\n"  );
+			try{
+				dvarFile.print(dvName + ":  " + df.format(ControlData.xasolver.getColumnActivity(s)) +"\n"  );
+			} catch (Exception e) {
+				dvarFile.print(dvName + ":  " + ControlData.xasolver.getColumnActivity(s) +"\n"  );
+			}
 		}
 		dvarFile.println();
 		dvarFile.println("/* Unweighted Dvar    */");	
 		for (String s : dvar_unweighted){
 			String dvName = String.format("%-35s", s);
-			dvarFile.print(dvName + ":  " + ControlData.xasolver.getColumnActivity(s) +"\n"  );
+			try{
+				dvarFile.print(dvName + ":  " + df.format(ControlData.xasolver.getColumnActivity(s)) +"\n"  );
+			} catch (Exception e) {
+				dvarFile.print(dvName + ":  " + ControlData.xasolver.getColumnActivity(s) +"\n"  );
+			}
+		}
+	}
+
+	private static void writeDvarValue_LPSOLVE(PrintWriter dvarFile, Set<String> dvar_effective) {
+		
+		Map<String, Dvar> dvMap = SolverData.getDvarMap();
+		Map<String, WeightElement> wtMap = SolverData.getWeightMap();
+		//Map<String, WeightElement> wtSlackSurplusMap = SolverData.getWeightSlackSurplusMap();
+		
+		ArrayList<String> dvar_weighted = new ArrayList<String>(wtMap.keySet());
+		dvar_weighted.addAll(ControlData.currModelDataSet.usedWtSlackSurplusList);
+		
+		ArrayList<String> dvar_unweighted = new ArrayList<String>(dvMap.keySet());
+		dvar_unweighted.removeAll(wtMap.keySet());
+		dvar_unweighted.removeAll(ControlData.currModelDataSet.usedWtSlackSurplusList);
+		
+		dvar_unweighted.retainAll(dvar_effective);
+		Collections.sort(dvar_weighted);
+		Collections.sort(dvar_unweighted);
+		
+		
+		dvarFile.println("/* Weighted Dvar    */");
+		for (String s : dvar_weighted){
+			String dvName = String.format("%-35s", s);
+			//dvarFile.print(dvName + ":  " + ControlData.xasolver.getColumnActivity(s) +"\n"  );
+			try{
+				dvarFile.print(dvName + ":  " + df.format(LPSolveSolver.varDoubleMap.get(s)) +"\n"  );
+			} catch (Exception e) {
+				dvarFile.print(dvName + ":  " + LPSolveSolver.varDoubleMap.get(s) +"\n"  );
+			}
+		}
+		dvarFile.println();
+		dvarFile.println("/* Unweighted Dvar    */");	
+		for (String s : dvar_unweighted){
+			String dvName = String.format("%-35s", s);
+			try{
+				dvarFile.print(dvName + ":  " + df.format(LPSolveSolver.varDoubleMap.get(s)) +"\n"  );
+			} catch (Exception e) {
+				dvarFile.print(dvName + ":  " + LPSolveSolver.varDoubleMap.get(s) +"\n"  );
+			}
 		}
 	}
 
@@ -374,10 +437,17 @@ public class ILP {
 		ArrayList<String> sortedTerm = new ArrayList<String>(svMap.keySet());
 		Collections.sort(sortedTerm);
 		
+
+
+		
 		for (String s : sortedTerm){
 			String svName = String.format("%-35s", s);
-			svarFile.print(svName + ":  " + svMap.get(s).getData().getData() +"\n"  );
+			svarFile.print(svName + ":  " + df.format(svMap.get(s).getData().getData()) +"\n"  );
+//			svarFile.print(svName + ":  ");
+//			svarFile.print(df.format(svMap.get(s).getData().getData()));
+//			svarFile.print("\n"  );
 			
 		}
+
 	}
 }
