0e0ccef65ba92dc2269f1d7f1220f1849a38ce3d
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-05-03 18:40:24 +0000

    update test

--- wrims_v2/wrims_v2/src/test/test_wreslplus/TestWreslPlus_alias.java
+++ wrims_v2/wrims_v2/src/test/test_wreslplus/TestWreslPlus_alias.java
@@ -159,4 +159,156 @@ public class TestWreslPlus_alias {
 		Assert.assertEquals(totalErrs, 0);		
 	
 	}
+
+	@Test(groups = { "WRESLPLUS_elements" })
+	public void alias_to_goal6() throws RecognitionException, IOException {
+		
+	
+		testName = TestParam.testNamePrepend + "_alias_to_goal6";
+		csvFolderPath = "testResult_wreslplus\\"+testName;
+		
+		inputFilePath = projectPath + testName + TestParam.fileExt;
+		logFilePath = csvFolderPath + ".log";
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		StudyTemp styTemp=Workflow.checkStudy(absFilePath);
+		
+		StudyDataSet sd = ToWreslData.convertStudy(styTemp);
+		
+		WriteCSV.study(sd, this.csvFolderPath);
+		
+		LogUtils.closeLogFile();
+	
+		String logText = Tools.readFileAsString(logFilePath);	
+		
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		
+//		// to match the missing dependants in wreslparser
+//		String csvText = Tools.readFileAsString(csvFolderPath+"\\first\\constraint.csv");	
+//		String csvText_modified = csvText.replaceAll("dv;", "");
+//		
+//		PrintWriter csvFile = Tools.openFile(System.getProperty("user.dir"), csvFolderPath+"\\first\\constraint.csv");
+//		csvFile.print(csvText_modified);
+//		csvFile.close();
+		
+		Assert.assertEquals(totalErrs, 0);		
+	
+	}
+
+	@Test(groups = { "WRESLPLUS_elements" })
+	public void alias_to_goal7() throws RecognitionException, IOException {
+		
+	
+		testName = TestParam.testNamePrepend + "_alias_to_goal7";
+		csvFolderPath = "testResult_wreslplus\\"+testName;
+		
+		inputFilePath = projectPath + testName + TestParam.fileExt;
+		logFilePath = csvFolderPath + ".log";
+		LogUtils.setLogFile(logFilePath);
+		
+		File absFile = new File(inputFilePath).getAbsoluteFile();
+		String absFilePath = absFile.getCanonicalPath().toLowerCase();
+		
+		StudyTemp styTemp=Workflow.checkStudy(absFilePath);
+		
+		StudyDataSet sd = ToWreslData.convertStudy(styTemp);
+		
+		WriteCSV.study(sd, this.csvFolderPath);
+		
+		LogUtils.closeLogFile();
+	
+		String logText = Tools.readFileAsString(logFilePath);	
+		
+		int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+		
+//		// to match the missing dependants in wreslparser
+//		String csvText = Tools.readFileAsString(csvFolderPath+"\\first\\constraint.csv");	
+//		String csvText_modified = csvText.replaceAll("dv;", "");
+//		
+//		PrintWriter csvFile = Tools.openFile(System.getProperty("user.dir"), csvFolderPath+"\\first\\constraint.csv");
+//		csvFile.print(csvText_modified);
+//		csvFile.close();
+		
+		Assert.assertEquals(totalErrs, 7);		
+	
+	}
+
+	@Test(groups = { "WRESLPLUS_elements" })
+		public void alias() throws RecognitionException, IOException {
+			
+		
+			testName = TestParam.testNamePrepend + "_alias";
+			csvFolderPath = "testResult_wreslplus\\"+testName;
+			
+			inputFilePath = projectPath + testName + TestParam.fileExt;
+			logFilePath = csvFolderPath + ".log";
+			LogUtils.setLogFile(logFilePath);
+			
+			File absFile = new File(inputFilePath).getAbsoluteFile();
+			String absFilePath = absFile.getCanonicalPath().toLowerCase();
+			
+			StudyTemp styTemp=Workflow.checkStudy(absFilePath);
+			
+			StudyDataSet sd = ToWreslData.convertStudy(styTemp);
+			
+			WriteCSV.study(sd, this.csvFolderPath);
+			
+			LogUtils.closeLogFile();
+		
+			String logText = Tools.readFileAsString(logFilePath);	
+			
+			int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+			
+	//		// to match the missing dependants in wreslparser
+	//		String csvText = Tools.readFileAsString(csvFolderPath+"\\first\\constraint.csv");	
+	//		String csvText_modified = csvText.replaceAll("dv;", "");
+	//		
+	//		PrintWriter csvFile = Tools.openFile(System.getProperty("user.dir"), csvFolderPath+"\\first\\constraint.csv");
+	//		csvFile.print(csvText_modified);
+	//		csvFile.close();
+			
+			Assert.assertEquals(totalErrs, 7);		
+		
+		}
+
+	@Test(groups = { "WRESLPLUS_elements" })
+		public void alias2() throws RecognitionException, IOException {
+			
+		
+			testName = TestParam.testNamePrepend + "_alias2";
+			csvFolderPath = "testResult_wreslplus\\"+testName;
+			
+			inputFilePath = projectPath + testName + TestParam.fileExt;
+			logFilePath = csvFolderPath + ".log";
+			LogUtils.setLogFile(logFilePath);
+			
+			File absFile = new File(inputFilePath).getAbsoluteFile();
+			String absFilePath = absFile.getCanonicalPath().toLowerCase();
+			
+			StudyTemp styTemp=Workflow.checkStudy(absFilePath);
+			
+			StudyDataSet sd = ToWreslData.convertStudy(styTemp);
+			
+			WriteCSV.study(sd, this.csvFolderPath);
+			
+			LogUtils.closeLogFile();
+		
+			String logText = Tools.readFileAsString(logFilePath);	
+			
+			int totalErrs = RegUtils.timesOfMatches(logText, "# Error");
+			
+	//		// to match the missing dependants in wreslparser
+	//		String csvText = Tools.readFileAsString(csvFolderPath+"\\first\\constraint.csv");	
+	//		String csvText_modified = csvText.replaceAll("dv;", "");
+	//		
+	//		PrintWriter csvFile = Tools.openFile(System.getProperty("user.dir"), csvFolderPath+"\\first\\constraint.csv");
+	//		csvFile.print(csvText_modified);
+	//		csvFile.close();
+			
+			Assert.assertEquals(totalErrs, 7);		
+		
+		}
 }
