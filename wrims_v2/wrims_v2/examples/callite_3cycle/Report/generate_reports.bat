e3adb54e190aeb9d4654b4b854bfea6e61179281
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-07-19 22:14:06 +0000

    update example

---
+++ wrims_v2/wrims_v2/examples/callite_3cycle/Report/generate_reports.bat
@@ -0,0 +1,12 @@
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_PREBO__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd FUTURE_BO__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd FUTURE_PREBO__(LpSolve_vs_XA).inp
+
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_XA__(local_vs_global).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_PREBO_XA__(local_vs_global).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_XA_local__(WeightGroup_vs_LegacyWresl).inp
+start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_local_WeightGroup_1265__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_local_WeightGroup_try2__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_local_WeightGroup_1264_1265_5000__(LpSolve_vs_XA).inp
+REM start /wait D:\cvwrsm\callite-report-tool\callite_report_batch.cmd EXISTING_BO_XA_local__(WeightGroup_1265_5000_vs_LegacyWresl).inp
