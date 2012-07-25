a9a2eb78c003fd2b2b8bea8d147c7119b8ab65f8
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-07-25 19:56:21 +0000

    add vargoal search tool/dialog

---
+++ wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/debugger/dialog/WPPVarGoalSearchDialog.java
@@ -0,0 +1,94 @@
+package wrimsv2_plugin.debugger.dialog;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.jface.dialogs.PopupDialog;
+import org.eclipse.jface.viewers.CellEditor.LayoutData;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+
+import wrimsv2_plugin.debugger.core.DebugCorePlugin;
+import wrimsv2_plugin.debugger.exception.WPPException;
+import wrimsv2_plugin.debugger.menuitem.EnableRunMenu;
+import wrimsv2_plugin.debugger.view.WPPVarDetailView;
+
+public class WPPVarGoalSearchDialog extends PopupDialog {
+	
+	private IViewPart view;
+	
+	public WPPVarGoalSearchDialog(Shell parent, int shellStyle,
+			boolean takeFocusOnOpen, boolean persistSize,
+			boolean persistLocation, boolean showDialogMenu,
+			boolean showPersistActions, String titleText, String infoText) {
+		super(parent, shellStyle, takeFocusOnOpen, persistSize, persistLocation,
+				showDialogMenu, showPersistActions, titleText, infoText);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void open(IViewPart view){
+		this.view=view;
+		create();
+		getShell().setSize(250, 150);
+		open();
+	}
+
+	@Override
+	 protected Control createDialogArea(Composite parent) {
+		Composite dialogArea = (Composite) super.createDialogArea(parent);
+		FillLayout layout=new FillLayout(SWT.VERTICAL);
+		layout.marginWidth=20;
+		dialogArea.setLayout(layout);
+		
+		Label label1=new Label(dialogArea, SWT.NONE);
+		label1.setText("Search:");
+		
+		Text text1=new Text(dialogArea, SWT.BORDER);
+				
+		RowLayout layout1=new RowLayout(SWT.HORIZONTAL);
+		layout1.fill=true;
+		//layout.pack=true;
+		Composite line3=new Composite(dialogArea, SWT.NONE);
+		line3.setLayout(layout1);
+		Button ok = new Button(line3, SWT.PUSH);
+		ok.setText("OK");
+		ok.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent event){
+				
+				close();
+			}
+		});
+		
+		Button cancel = new Button(line3, SWT.PUSH);
+		cancel.setText("Cancel");
+		cancel.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent event){
+				close();
+			}
+		});
+		
+		return dialogArea;
+	 }
+}
