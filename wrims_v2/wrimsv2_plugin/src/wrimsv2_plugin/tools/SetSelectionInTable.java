c059d6d7806e7448bc45bd641158f1029a7af450
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-08-16 15:22:40 +0000

    select table items after table updated

--- wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/tools/SetSelectionInTable.java
+++ wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/tools/SetSelectionInTable.java
@@ -1,5 +1,8 @@
 package wrimsv2_plugin.tools;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Table;
@@ -9,18 +12,32 @@ import wrimsv2_plugin.debugger.model.WPPValue;
 public class SetSelectionInTable {
 	public SetSelectionInTable(IStructuredSelection oldSelection, TableViewer viewer, Table table){
 		boolean hasOldSelection=false;
+		ArrayList<String> oldSelections = new ArrayList<String>();
+		ArrayList<Integer> oldSelectionIndices=new ArrayList<Integer>();
     	int i=0;
     	if (oldSelection.isEmpty()){
     		table.setTopIndex(0);
     	}else{
-    		String oldVarString = ((WPPValue)(oldSelection.getFirstElement())).getVariableString();
+    		Iterator iterator = oldSelection.iterator();
+    		while (iterator.hasNext()){
+    			oldSelections.add(((WPPValue)(iterator.next())).getVariableString());
+    		}
     		Object element;
-    		while (!hasOldSelection && (element=viewer.getElementAt(i))!=null ){
-    			if (oldVarString.equals(((WPPValue)element).getVariableString())) hasOldSelection=true;
+    		while ((element=viewer.getElementAt(i))!=null ){
+    			if (oldSelections.contains(((WPPValue)element).getVariableString())){
+    				hasOldSelection=true;
+        			oldSelectionIndices.add(i);
+    			}
     			i=i+1;
     		}
     		if (hasOldSelection){
-    			table.setTopIndex(i-1);
+    			int size = oldSelectionIndices.size();
+    			int[] indices=new int[size];
+    			for (int j=0; j<size; j++){
+    				indices[j]=oldSelectionIndices.get(j);
+    			}
+    			table.select(indices);
+    			table.setTopIndex(oldSelectionIndices.get(0));
     		}else{
     			table.setTopIndex(0);
     		}
