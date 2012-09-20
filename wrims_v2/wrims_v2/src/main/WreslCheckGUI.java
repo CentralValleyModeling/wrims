3193d7cd5af55f0dbe7867393fb0cd19786aa756
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-09-20 17:38:30 +0000

    update WreslChecker to include wreslplus

--- wrims_v2/wrims_v2/src/main/WreslCheckGUI.java
+++ wrims_v2/wrims_v2/src/main/WreslCheckGUI.java
@@ -15,8 +15,12 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import wrimsv2.commondata.wresldata.Param;
+import wrimsv2.components.ControlData;
 import wrimsv2.components.Versions;
+import wrimsv2.wreslparser.elements.LogUtils;
+import wrimsv2.wreslparser.elements.StudyParser;
 import wrimsv2.wreslparser.elements.StudyUtils;
+import wrimsv2.wreslplus.elements.Workflow;
 
 public class WreslCheckGUI {
 	
@@ -33,7 +37,8 @@ public class WreslCheckGUI {
 		//Font displayFont = new Font("Consolas", Font.BOLD, 14);
 		
 		final JButton runButton = new JButton("Run Wresl Checker");
-		final JCheckBox chk_csv =new JCheckBox("Generate report");	
+		final JCheckBox chk_wreslPlus =new JCheckBox("WreslPlus");	
+		//final JCheckBox chk_csv =new JCheckBox("Generate report");	
 		final JTextArea text_parsingTime = new JTextArea(" ");
 //		final JProgressBar progressBar = new JProgressBar(0);
 //		
@@ -42,10 +47,12 @@ public class WreslCheckGUI {
 //		progressBar.setStringPainted(true);
 //		
 		//runButton.setFont(displayFont);
-		chk_csv.setSelected(false);
+		chk_wreslPlus.setSelected(true);
+		//chk_csv.setSelected(true);
 		text_parsingTime.setEditable(false);
 		
-		wreslCheckPanel.add(chk_csv);
+		wreslCheckPanel.add(chk_wreslPlus);
+		//wreslCheckPanel.add(chk_csv);
 		wreslCheckPanel.add(runButton);
 //		wreslCheckPanel.add(progressBar);
 		wreslCheckPanel.add(text_parsingTime);
@@ -62,30 +69,43 @@ public class WreslCheckGUI {
 				SwingUtilities.invokeLater(new Runnable() {
 					public void run() {				
 						
+						ControlData.outputWreslCSV=true;
+						
 						try {
 							long start = System.currentTimeMillis();
 							
-							if (chk_csv.isSelected()){
-								StudyUtils.checkStudy(inputFilePanel.getFile(), "=WreslCheck_csv=", false);
-							} else {
-								StudyUtils.checkStudy(inputFilePanel.getFile(), null, false);
-							}
+							if (chk_wreslPlus.isSelected()){
+								
+								StudyUtils.useWreslPlus = true;
+									
+							} 
+
+							StudyUtils.checkStudy(inputFilePanel.getMainFilePath());
 							
-							
-							float elapsedSec = (System.currentTimeMillis()-start)/1000F;
+//							if (chk_csv.isSelected()) {
+//								StudyUtils.checkStudy(inputFilePanel.getFile(), "=WreslCheck_csv=", false);
+//							}
+//							else {
+//								StudyUtils.checkStudy(inputFilePanel.getFile(), null, false);
+//							}
+
+							float elapsedSec = (System.currentTimeMillis() - start) / 1000F;
 							String elapsedSecStr = Float.toString(elapsedSec);
-							text_parsingTime.setText("Total errors: " + StudyUtils.total_errors +"; Parsing time in seconds: "+elapsedSecStr);
-							
+							text_parsingTime.setText("Total errors: " + StudyUtils.total_errors
+									+ "; Parsing time in seconds: " + elapsedSecStr);
 
 						} catch (Exception ex) {
+							
+							ex.printStackTrace();
+							
 							text_parsingTime.setText(" ");
 							JOptionPane.showMessageDialog(runButton, ex
-									.getMessage(), "Error",
+									.getMessage(), "Error!",
 									JOptionPane.ERROR_MESSAGE);
 						} finally {
 							runButton.setText("Run Wresl Checker");
 							runButton.setEnabled(true);
-							chk_csv.setSelected(false);
+							//chk_csv.setSelected(false);
 							
 						}
 					}
@@ -129,7 +149,7 @@ public class WreslCheckGUI {
 			});
 		}
 
-		public String getFile() {
+		public String getMainFilePath() {
 			return textfield.getText();
 		}
 	}
