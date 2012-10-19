24beaceb7ee685b9b47e7ff50ae391ee17728556
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-10-19 23:51:45 +0000

    minor. fix errors in test.

--- wresl++/examples/WeightGroupDemo/test_run.bat
+++ wresl++/examples/WeightGroupDemo/test_run.bat
@@ -2,3 +2,5 @@ start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\W
 start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo1.config
 start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo2.config
 start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo3.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo3_error.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_more1.config
