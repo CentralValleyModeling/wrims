b71a980079daeb79d0e0d5636b64bc82625b3bbb
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-10-25 23:31:08 +0000

    update test

--- wresl++/examples/WeightGroupDemo/test_run.bat
+++ wresl++/examples/WeightGroupDemo/test_run.bat
@@ -1,6 +1,5 @@
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo0.config
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo1.config
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo2.config
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo3.config
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo3_error.config
-start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_more1.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_1.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_1_wreslplus.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_2.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_2_wreslplus_error.config
+start ..\runConfigDev_limitedXA.bat J:\cvwrsm\trunk\wrims_v2\wrims_v2\examples\WeightGroupDemo\demo_2_wreslplus_revised.config
