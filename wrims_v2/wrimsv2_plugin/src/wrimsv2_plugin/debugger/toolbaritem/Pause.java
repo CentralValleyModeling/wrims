7e88fac935ad971e7ca26c464566f09034fb886e
water\hxie <water\hxie@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2012-12-26 17:09:35 +0000

    add pause toolbar button

---
+++ wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/debugger/toolbaritem/Pause.java
@@ -0,0 +1,71 @@
+package wrimsv2_plugin.debugger.toolbaritem;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionDelegate;
+
+import wrimsv2_plugin.debugger.core.DebugCorePlugin;
+import wrimsv2_plugin.debugger.dialog.WPPAddWatchDialog;
+import wrimsv2_plugin.debugger.dialog.WPPReSimDialog;
+import wrimsv2_plugin.debugger.dialog.WPPVarGoalSearchDialog;
+import wrimsv2_plugin.debugger.exception.WPPException;
+import wrimsv2_plugin.debugger.menuitem.EnableMenus;
+import wrimsv2_plugin.debugger.view.WPPAllGoalView;
+import wrimsv2_plugin.debugger.view.WPPAllVariableView;
+import wrimsv2_plugin.debugger.view.WPPVariableView;
+import wrimsv2_plugin.tools.DataProcess;
+
+public class Pause extends ActionDelegate implements IViewActionDelegate{
+	IViewPart view;
+	
+	@Override
+	public void init(IViewPart view) {
+		this.view=view;		
+	}
+
+	public void run(IAction action) {
+		try {
+			if (DebugCorePlugin.isDebugging){
+				DebugCorePlugin.target.pause();
+				enableRunMenu();
+			}
+		} catch (DebugException e) {
+			WPPException.handleException(e);
+		}
+	}
+	
+	public void enableRunMenu(){
+		HashMap<String, Boolean> enableMap=new HashMap<String, Boolean>();
+		enableMap.put(DebugCorePlugin.ID_WPP_TERMINATEMENU, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_PAUSEMENU, false);
+		enableMap.put(DebugCorePlugin.ID_WPP_SUSPENDMENU, false);
+		enableMap.put(DebugCorePlugin.ID_WPP_RESUMEMENU, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_RESIMMENU, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_NEXTCYCLE, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_NEXTTIMESTEP, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_SAVETODVFILE, true);
+		enableMap.put(DebugCorePlugin.ID_WPP_SAVETOSVFILE, true);
+		new EnableMenus(enableMap);
+	}
+}
