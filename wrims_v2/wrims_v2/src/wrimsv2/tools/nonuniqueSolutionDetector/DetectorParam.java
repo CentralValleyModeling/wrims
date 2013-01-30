791ed461f5efb9503a73138e88c1da44cc86d905
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2013-01-30 16:40:47 +0000

    move parameters to a new class

---
+++ wrims_v2/wrims_v2/src/wrimsv2/tools/nonuniqueSolutionDetector/DetectorParam.java
@@ -0,0 +1,13 @@
+
+package wrimsv2.tools.nonuniqueSolutionDetector;
+
+
+public class DetectorParam {
+
+	public static final int altSolutionFound = 1;
+	public static final int altSolutionNotFound = 0;	
+	public static final int modelNotValid = -1;
+	public static final int otherErrors = -2;
+
+
+}
