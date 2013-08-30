a060604ce1ae8d48b62c42de640f112487f36de0
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2013-08-30 20:28:50 +0000

    add user preference class

--- wrims2_vscript/dev/src/wvscript/app/WrimsStudy.java
+++ wrims2_vscript/dev/src/wvscript/app/WrimsStudy.java
@@ -1,18 +1,21 @@
 package wvscript.app;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WrimsStudy {
 
-	public String studyRunDir;
-	public String studyName;
+	public File studyRunDir;
+	public File configFile;
+	//public String studyName;
 	public Map<String,String> configMap;
 	
 	public WrimsStudy() {
 
-		this.studyRunDir = "";
-		this.studyName = "";
+		this.studyRunDir = null;
+		this.configFile = null;
+		//this.studyName = "";
 		configMap = new HashMap<String,String>();
 		
 	}
