e905cd625138afe406aa352677beeb85fa40f163
water\kkao <water\kkao@62c3354f-a8e3-c54a-9df1-72d55bbaa0c9>
2014-10-03 21:20:05 +0000

    update vista lib to double precision.

--- wrims2_vscript/vscript.bat
+++ wrims2_vscript/vscript.bat
@@ -2,26 +2,49 @@
 rem ##############################
 rem Batch file for running vscript
 rem ##############################
-
-set lib_home=%~dp0/lib/
-set vista_home=%lib_home%/vista/
-set hec_home=%lib_home%/hecdss/
-set wrims2_home=%lib_home%/wrims2/
-set misc_home=%lib_home%/misc/
+set vista_home=%~dp0/lib/vista_hec
 set jre_home=%~dp0/jre/
+
+if exist "%vista_home%/jython/jython.jar" goto :valid
+
+
+:notfound
+
+echo ############################################################
+echo   Error: VISTA files not found
+echo   ___
+echo   Installation instructions
+echo   ___
+echo   The value of the environment variable vista_home in the 
+echo   file vscript.bat needs to match the location where
+echo   VISTA has been installed
+echo ############################################################
+PAUSE
+goto :end
+
+:valid
 
 setlocal
 rem ###############
 rem Set path to location of dll
 rem ###############
 
-set PATH=%path%;%vista_home%/bin;%vista_home%/lib;%wrims2_home%;
+set wrims2_home=%~dp0/lib/wrims2
+set misc_home=%~dp0/lib/misc
 
-set PYPATH="%vista_home%/jython/Lib;%vista_home%/lib/Lib;%hec_home%/hec.jar;%hec_home%/hecData.jar;%hec_home%/heclib.jar;%hec_home%/rma.jar;"
+set PATH=%path%;%vista_home%/bin;%vista_home%/lib;
 
-set CPATH="%vista_home%/lib/vista.jar;%vista_home%/jython/jython.jar;%vista_home%/lib/jakarta-oro-2.0.8.jar;%vista_home%/lib/pd.jar;%vista_home%/lib/misc.jar;%vista_home%/lib/jhall.jar;%vista_home%/lib/jnios.jar;%vista_home%/lib/widgets.jar;%wrims2_home%/WRIMSv2.jar;%misc_home%/wvscript.jar"
+set PYPATH="%vista_home%/jython/Lib;%vista_home%/lib/Lib;%DSM2_HOME%/scripts"
 
-set LPATH="%vista_home%/lib;%hec_home%" -Dvista.home="%vista_home%"
+set CPATH="%vista_home%/lib/vista.jar;%vista_home%/lib/vista-help.jar;%vista_home%/jython/jython.jar;%vista_home%/lib/pd.jar;%vista_home%/lib/misc.jar;%vista_home%/lib/jhall.jar;%vista_home%/lib/jnios.jar;%vista_home%/lib/widgets.jar;%vista_home%/lib/jhdf5.jar;%vista_home%/lib/jhdf5obj.jar;%vista_home%/lib/jhdfobj.jar;%vista_home%/lib/heclib.jar;%vista_home%/lib/dsm2-input-model.jar;%vista_home%/lib/ojdbc6.jar"
+
+set CPATH="%vista_home%/lib/vista.jar;%vista_home%/lib/vista-help.jar;%vista_home%/jython/jython.jar;%vista_home%/lib/pd.jar;%vista_home%/lib/misc.jar;%vista_home%/lib/jhall.jar;%vista_home%/lib/jnios.jar;%vista_home%/lib/widgets.jar;%vista_home%/lib/jhdf5.jar;%vista_home%/lib/jhdf5obj.jar;%vista_home%/lib/jhdfobj.jar;%vista_home%/lib/dsm2-input-model.jar;%vista_home%/lib/ojdbc6.jar"
+
+set HECPATH=%vista_home%/lib/heclib.jar;%vista_home%/lib/hec.jar;%vista_home%/lib/hecData.jar;%vista_home%/lib/rma.jar;%vista_home%/lib/dssvueHelp.jar;%vista_home%/lib/images.jar;
+
+set CPATH=%CPATH%;%HECPATH%;"%wrims2_home%/WRIMSv2.jar;%misc_home%/wvscript.jar"
+
+set LPATH="%vista_home%/lib" 
 
 set PYHOME="%vista_home%/jython"
 
@@ -41,11 +64,11 @@ rem ###############
 if defined ARGS goto run2
 
 :run1
-%jre_home%\bin\java -Xmx800m  -Djava.library.path=%LPATH% -Dvista.home="%vista_home%" -Dpython.home=%PYHOME% -Dpython.path=%PYPATH% -classpath %CPATH% org.python.util.jython -i "%vista_home%/lib/__init__.py"
+"%jre_home%\bin\java" -mx800m  -Djava.library.path=%LPATH% -Dvista.home="%vista_home%" -Dpython.home=%PYHOME% -Dpython.path=%PYPATH% -classpath %CPATH% org.python.util.jython -i "%vista_home%/lib/__init__.py"
 goto end
 
 :run2
-%jre_home%\bin\java -Xmx800m  -Djava.library.path=%LPATH% -Dvista.home="%vista_home%" -Dpython.home=%PYHOME% -Dpython.path=%PYPATH% -classpath %CPATH% org.python.util.jython -i  %ARGS%
+"%jre_home%\bin\java" -mx800m  -Djava.library.path=%LPATH% -Dvista.home="%vista_home%" -Dpython.home=%PYHOME% -Dpython.path=%PYPATH% -classpath %CPATH% org.python.util.jython -i  %ARGS%
 
 endlocal
 :end 
