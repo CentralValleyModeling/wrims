0970869018efa734e3ecf7517393527c479c96b0
Hao Xie <hxie@water.ca.gov>
2016-02-23 10:13:01 -0800

    force terminate

--- wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/debugger/launcher/WPPLaunchDelegate.java
+++ wrims_v2/wrimsv2_plugin/src/wrimsv2_plugin/debugger/launcher/WPPLaunchDelegate.java
@@ -136,6 +136,7 @@ public class WPPLaunchDelegate extends LaunchConfigurationDelegate {
 				IProcess p = DebugPlugin.newProcess(launch, process, "RunWPP");
 				process.waitFor();
 				terminateCode=process.exitValue();
+				Runtime.getRuntime().exec("wmic Process Where \"CommandLine Like '%-Dname="+requestPort+"%'\" Call Terminate");
 			}
 		} catch (Exception e) {
 			WPPException.handleException(e);
@@ -185,6 +186,7 @@ public class WPPLaunchDelegate extends LaunchConfigurationDelegate {
 					process.waitFor();
 					terminateCode=process.exitValue();
 					process.destroy();
+					Runtime.getRuntime().exec("wmic Process Where \"CommandLine Like '%-Dname="+requestPort+"%'\" Call Terminate");
 				}
 			} catch (Exception e) {
 				WPPException.handleException(e);
@@ -452,7 +454,7 @@ public class WPPLaunchDelegate extends LaunchConfigurationDelegate {
 		if (mode.equals("debug")){
 			out.println("jre\\bin\\java -Xmx1600m -Xss1024K -Duser.timezone=UTC -Djava.library.path=\"" + externalPath + ";lib\" -cp \""+externalPath+";"+"lib\\external;lib\\WRIMSv2.jar;lib\\commons-io-2.1.jar;lib\\"+jarXA+";lib\\lpsolve55j.jar;lib\\gurobi.jar;lib\\heclib.jar;lib\\jnios.jar;lib\\jpy.jar;lib\\misc.jar;lib\\pd.jar;lib\\vista.jar;lib\\guava-11.0.2.jar;lib\\javatuples-1.2.jar;lib\\kryo-2.24.0.jar;lib\\minlog-1.2.jar;lib\\objenesis-1.2.jar;\" wrimsv2.components.DebugInterface "+requestPort+" "+eventPort+" "+"-config="+configFilePath);
 		}else{
-			out.println("jre\\bin\\java -Xmx1600m -Xss1024K -Duser.timezone=UTC -Djava.library.path=\"" + externalPath + ";lib\" -cp \""+externalPath+";"+"lib\\external;lib\\WRIMSv2.jar;lib\\commons-io-2.1.jar;lib\\"+jarXA+";lib\\lpsolve55j.jar;lib\\gurobi.jar;lib\\heclib.jar;lib\\jnios.jar;lib\\jpy.jar;lib\\misc.jar;lib\\pd.jar;lib\\vista.jar;lib\\guava-11.0.2.jar;lib\\javatuples-1.2.jar;lib\\kryo-2.24.0.jar;lib\\minlog-1.2.jar;lib\\objenesis-1.2.jar;\" wrimsv2.components.ControllerBatch "+"-config="+configFilePath);
+			out.println("jre\\bin\\java -Xmx1600m -Xss1024K -Duser.timezone=UTC -Dname="+requestPort+" -Djava.library.path=\"" + externalPath + ";lib\" -cp \""+externalPath+";"+"lib\\external;lib\\WRIMSv2.jar;lib\\commons-io-2.1.jar;lib\\"+jarXA+";lib\\lpsolve55j.jar;lib\\gurobi.jar;lib\\heclib.jar;lib\\jnios.jar;lib\\jpy.jar;lib\\misc.jar;lib\\pd.jar;lib\\vista.jar;lib\\guava-11.0.2.jar;lib\\javatuples-1.2.jar;lib\\kryo-2.24.0.jar;lib\\minlog-1.2.jar;lib\\objenesis-1.2.jar;\" wrimsv2.components.ControllerBatch "+"-config="+configFilePath);
 		}
 		out.close();
 	}
