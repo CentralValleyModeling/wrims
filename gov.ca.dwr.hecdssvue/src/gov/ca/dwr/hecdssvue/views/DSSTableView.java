f02b006c4df7c02e29a3f8c712ccb8bf9b27fada
Hao Xie <hxie@water.ca.gov>
2016-03-02 11:15:16 -0800

    minor

--- gov.ca.dwr.hecdssvue/src/gov/ca/dwr/hecdssvue/views/DSSTableView.java
+++ gov.ca.dwr.hecdssvue/src/gov/ca/dwr/hecdssvue/views/DSSTableView.java
@@ -46,12 +46,13 @@ public class DSSTableView extends AbstractDSSView {
 	public void showSelected(Vector<DataContainer> dataVector) {
 		if (table !=null) DataOps.saveData(table);
 		
-		if (dataVector != null && dataVector.size() > 0) {
-			for (int i=dataVector.size()-1; i>=0; i--){
-				if (dataVector.get(i)==null){
-					dataVector.remove(i);
-				}
+		for (int i=dataVector.size()-1; i>=0; i--){
+			if (dataVector.get(i)==null){
+				dataVector.remove(i);
 			}
+		}
+		
+		if (dataVector != null && dataVector.size() > 0) {
 			contentPane.removeAll();
 			table = new HecDataTable(contentPane);
 			table.setDateTimeAsTwoColumns(false);
