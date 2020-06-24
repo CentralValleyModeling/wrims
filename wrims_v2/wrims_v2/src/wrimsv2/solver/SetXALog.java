9fb6e255b20ff6e3e29558918983b522a7255051
Hao Xie <hxie@water.ca.gov>
2020-06-24 09:03:34 -0700

    allow XA configuration specification

--- wrims_v2/wrims_v2/src/wrimsv2/solver/SetXALog.java
+++ wrims_v2/wrims_v2/src/wrimsv2/solver/SetXALog.java
@@ -1,12 +1,36 @@
 package wrimsv2.solver;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import wrimsv2.components.ControlData;
 import wrimsv2.components.FilePaths;
 
 public class SetXALog {
 	public static void enableXALog(){
-		ControlData.xasolver.setCommand( "set debug no ToRcc Yes FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName YES MPSX Yes Set FreqLog :01") ;
-		//ControlData.xasolver.setCommand("When +10920 FileName d:\\xatemp\\xa%d ToRcc Yes Output xa%d set sortName YES MPSX Yes");
+		File config=new File ("xa_config.dat");
+		if (config.exists()){
+			BufferedReader br;
+			try {
+				FileReader fr=new FileReader(config);
+				br = new BufferedReader(fr);
+				String line = br.readLine();
+				System.out.println("Retrieve XA configuation from xa_config.dat file");
+				ControlData.xasolver.setCommand( "set debug no ToRcc Yes FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log "+line);
+				br.close();
+				fr.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				ControlData.xasolver.setCommand( "set debug no ToRcc Yes FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName YES MPSX Yes Set FreqLog :01") ;
+			} 
+			
+		}else{
+			ControlData.xasolver.setCommand( "set debug no ToRcc Yes FileName  "+FilePaths.mainDirectory+"  Output "+FilePaths.mainDirectory+"\\xa.log set sortName YES MPSX Yes Set FreqLog :01") ;
+			//ControlData.xasolver.setCommand("When +10920 FileName d:\\xatemp\\xa%d ToRcc Yes Output xa%d set sortName YES MPSX Yes");
+		}
 	}
 	
 	public static void disableXALog(){
