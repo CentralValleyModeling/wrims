9bb9131e97c32f47e07a10ec90fd676340248cf3
kuckaogh <kkao.office@gmail.com>
2020-06-26 16:00:42 -0700

    fix cbcdebug minor error and add packaging script

---
+++ wrims2_compact/distro/distro.bat
@@ -0,0 +1,14 @@
+set s=..\..\wrims2_compact
+set t=z:\wrims2_compact_20200626a
+
+robocopy %s% %t% runConfig.bat runConfig_pause.bat test_different_study.bat test_same_study.bat
+
+robocopy %s%\example %t%\example /E /XD =ILP= /xf *.par *.log *.dsc *.dsd *.dsk *_dv.dss
+
+robocopy %s%\lib_64 %t%\lib_64 /E /XD other
+
+robocopy %s%\jars %t%\jars /E /xf *_old*
+
+robocopy %s%\jre8_x64 %t%\jre8_x64 /E
+
+rem robocopy ..\..\wrims2_compact %t% /e /XD distro =ILP= others examples jre8* /xf "examples\*" 
