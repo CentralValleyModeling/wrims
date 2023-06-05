eeb415ada88023981db8a9ddd8ba3fbe2e00c4e3
HamedZamanisabzi <129342615+HamedZamanisabzi@users.noreply.github.com>
2023-06-05 12:07:26 -0700

    Add files via upload
    This is a code representing an example of calculating the required Delta Outflow based on transformed data exported from ANN model trained within the Python environmentto the CALSIM within the WRIMS java Environment.

--- wrims_v2/wrims_v2/src/wrimsv2/tf/TF.java
+++ wrims_v2/wrims_v2/src/wrimsv2/tf/TF.java
@@ -1,55 +1,37 @@
 package wrimsv2.tf;
 
-import java.io.File;
-import java.util.Iterator;
-
-import org.tensorflow.Graph;
-import org.tensorflow.Operation;
 import org.tensorflow.SavedModelBundle;
 import org.tensorflow.Session;
 import org.tensorflow.Session.Runner;
 import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 
-
-
 public class TF {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-	    SavedModelBundle model = SavedModelBundle.load("Z:\\TensorFlow\\model", "serve");
-        Iterator<Operation> ops = model.graph().operations();
-        while (ops.hasNext()){
-        	Operation op = ops.next();
-        	System.out.println(op.name());
-        }
-	    Session s = model.session();
-        Tensor data = Tensor.create(new float[] {0.7228964f, 1.05657969f, 1.32106489f, 0.75237382f, 0.86339624f,
-        		-0.21546655f, -0.17141414f, -0.38003647f, -0.65384933f, -0.77411415f,
-        		-0.89288707f, -0.69964324f, -0.77795826f, -0.7921897f, -0.81271535f,
-        		0.f        ,  0.f        ,  0.f        ,  1.f        ,  0.f        ,
-        		0.f        ,  0.f        ,  0.f        ,  0.f        ,  1.f        ,
-        		0.f        ,  0.f        ,  0.f        ,  0.f        ,  0.f        ,
-        		0.f        ,  0.f       });
-        Runner runner = s.runner().feed("dense/kernel/Read/ReadVariableOp", data);
-        Tensor result = runner.fetch("total_1/Read/ReadVariableOp").run().get(0);
-        System.out.println("results: "+result.floatValue());
+        System.out.println("TensorFlow version: " + TensorFlow.version());
+
+        SavedModelBundle model = SavedModelBundle.load("D:\\TensorFlow\\ANN_Models1\\calsim_delta_outflow_estimator", "serve");
+        Session s = model.session();
+        float[][] rawdata = new float[][] { { -0.317928304f,	0.053429297f,	0.291882696f,	0.16009989f,	0.347007481f,	
+        	-0.575868808f,	0.159116005f,	0.132040745f,	0.11235005f,	0.470868431f,	
+        	-0.69574763f,	0.165035588f,	-0.492836345f,	-0.485022216f,	-0.454307444f,	
+        	1.f,	0.f,	0.f,	0.f,	0.f,	
+        	0.f,	0.f,	0.f,	0.f,	0.f,
+        	0.f,	0.f,	1.f,	0.f,	0.f,	0.f,	0.f} };
+           
         
-        model.close();	
-		
-		/*
-		// load the model
-		String simpleMlp = "Z:\\TensorFlow\\anntf.h5";
-		MultiLayerNetwork model = KerasModelImport.
-		                    importKerasSequentialModelAndWeights(simpleMlp);
-		// make a random sample
-		int inputs = 10;
-		INDArray features = Nd4j.zeros(inputs);
-		for (int i=0; i<inputs; i++) 
-		    features.putScalar(new int[] {i}, Math.random() < 0.5 ? 0 : 1);
-		// get the prediction
-		double prediction = model.output(features).getDouble(0);
-		*/
+        Tensor<Float> input = Tensor.create(rawdata, Float.class);
+        long[] sx = input.shape();
+        System.out.println("input shape: " + sx[0] + " " + sx[1]);
+        // Tensor.create([null,32], FloatBuffer.wrap(null))
+        Runner runner = s.runner().feed("serving_default_input_preprocessed_input:0", input);
+        Tensor result = runner.fetch("StatefulPartitionedCall:0").run().get(0);
+        float[][] out = new float[1][1];
+        result.copyTo(out);
+        System.out.println("results: " + out[0][0]);
+        model.close();
     }
 
 }
