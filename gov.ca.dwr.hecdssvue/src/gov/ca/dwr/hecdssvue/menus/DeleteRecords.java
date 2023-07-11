41471b75df6e128c3b0860f27f7d4498c220a9b1
Adam Korynta <adam@rmanet.com>
2023-07-11 15:54:52 -0700

    get eclipse projects compiling

--- gov.ca.dwr.hecdssvue/src/gov/ca/dwr/hecdssvue/menus/DeleteRecords.java
+++ gov.ca.dwr.hecdssvue/src/gov/ca/dwr/hecdssvue/menus/DeleteRecords.java
@@ -1,28 +1,15 @@
 package gov.ca.dwr.hecdssvue.menus;
 
-import java.util.Vector;
-
-import gov.ca.dwr.hecdssvue.DssPluginCore;
 import gov.ca.dwr.hecdssvue.components.DataOps;
-import gov.ca.dwr.hecdssvue.views.DSSCatalogView;
-import gov.ca.dwr.hecdssvue.views.DSSTableView;
-import hec.dataTable.HecDataTable;
-import hec.dssgui.DataReferenceSet;
-import hec.heclib.dss.HecDss;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
-
-import wrimsv2_plugin.debugger.core.DebugCorePlugin;
-import wrimsv2_plugin.debugger.view.WPPVarMonitorView;
 
 public class DeleteRecords implements IWorkbenchWindowActionDelegate{
 
